dfab99a888fedeef37a0fd95dbea643c63391ef1
Maks-gaming <77161824+Maks-gaming@users.noreply.github.com>
2023-11-02 14:19:18 +0300

    fix main on client

--- src/client/main.client.ts
+++ src/client/main.client.ts
@@ -3,24 +3,20 @@ import PlayerGameUI from "./gui/PlayerGameUI";
 import AliveEventsHandler from "./event/AliveEventsHandler";
 import ClientUnitTests from "client/tests/ClientUnitTests";
 
-function initialize() {
-	Players.LocalPlayer.CharacterAdded.Connect((_) => {
-		AliveEventsHandler.initialize();
-
-		PlayerGameUI.initialize();
-	});
+function initializeGUI() {
+	AliveEventsHandler.initialize();
+	PlayerGameUI.initialize();
 }
-// TODO: CHANGE SROCHNO
-AliveEventsHandler.initialize();
 
-PlayerGameUI.initialize();
+function initialize() {
+	Players.LocalPlayer.CharacterAdded.Connect((_) => initializeGUI());
+	initializeGUI(); // First start
+}
 
-// Start all scripts
 initialize();
 
-wait(5);
-
 // Unit tests
+wait(5);
 if (RunService.IsStudio() === true) {
 	ClientUnitTests.runClient();
 }
