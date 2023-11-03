b1bb937782ce47ef930e1680eec1cfead9712472
Maks-gaming <77161824+Maks-gaming@users.noreply.github.com>
2023-11-04 01:39:46 +0300

    beta testers only

--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -1,6 +1,7 @@
 import ServerPlots from "./building/ServerPlots";
 import BuildEvent from "./building/BuilldEvent";
 import DeleteEvent from "./building/DeleteEvent";
+import { Players } from "@rbxts/services";
 
 // Plots
 ServerPlots.initialize();
@@ -8,3 +9,12 @@ ServerPlots.initialize();
 // Initializing client-side components
 BuildEvent.initialize();
 DeleteEvent.initialize();
+
+// Testing whitelist
+Players.PlayerAdded.Connect((plr) => {
+	if (plr.IsInGroup(1088368) && plr.GetRankInGroup(1088368) < 2) {
+		return;
+	}
+
+	plr.Kick("Closed testing");
+});
