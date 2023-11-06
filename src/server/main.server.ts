dbf90bc0f392d66e1d98167f5ec327baeaf8be72
Maks-gaming <77161824+Maks-gaming@users.noreply.github.com>
2023-11-06 10:31:59 +0300

    PlotDatabase WIP

--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -2,8 +2,10 @@ import ServerPlots from "./plots/ServerPlots";
 import BuildEvent from "./network/event/BuildEvent";
 import DeleteEvent from "./network/event/DeleteEvent";
 import RideStartEvent from "./network/event/RideStartEvent";
+import PlotDatabase from "./plots/PlotDatabase";
 
 // Plots
+PlotDatabase.initialize();
 ServerPlots.initialize();
 
 // Initializing event workders
