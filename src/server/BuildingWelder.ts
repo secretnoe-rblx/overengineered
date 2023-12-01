df44c56c6acc8e9e0b07902b7ed54b2dd467d050
i3ym <20320769+i3ym@users.noreply.github.com>
2023-12-01 18:59:32 +0700

    Add raycasting to welder

--- src/server/BuildingWelder.ts
+++ src/server/BuildingWelder.ts
@@ -1,81 +1,157 @@
+import { Workspace } from "@rbxts/services";
+import { Visualize } from "@rbxts/visualize";
+
 export default class BuildingWelder {
-	private static getClosestParts(model: Model): BasePart[] {
-		const results: BasePart[] = [];
-		const vectors = this.getVectors(model);
-		const primaryPart = model.PrimaryPart as BasePart;
-		const canCollide = primaryPart.CanCollide;
+	static readonly points: Vector3[] = [];
 
-		if (!canCollide) {
-			primaryPart.CanCollide = true;
-		}
+	static {
+		Visualize.configure({
+			enabled: true,
+			alwaysOnTop: false,
+		});
 
-		for (let i = 0; i < vectors.size(); i++) {
-			this.checkVectorAndAddParts(model, primaryPart, vectors[i], results);
-		}
-
-		if (!canCollide) {
-			primaryPart.CanCollide = false;
-		}
-
-		return results;
-	}
-
-	private static getVectors(model: Model): Vector3[] {
-		const size = model.GetExtentsSize();
-		return [
-			new Vector3(0, size.Y * 0.1, 0),
-			new Vector3(0, 0, size.Z * 0.1),
-			new Vector3(size.X * 0.1, 0, 0),
-			new Vector3(0, -size.Y * 0.1, 0),
-			new Vector3(0, 0, -size.Z * 0.1),
-			new Vector3(-size.X * 0.1, 0, 0),
-		];
-	}
-
-	private static checkVectorAndAddParts(
-		model: Model,
-		primaryPart: BasePart,
-		vector: Vector3,
-		results: BasePart[],
-	): void {
-		const worldSpaceMovement = model.GetPivot().PointToWorldSpace(vector).sub(model.GetPivot().Position);
-		model.PivotTo(model.GetPivot().add(worldSpaceMovement));
-
-		primaryPart.GetTouchingParts().forEach((basepart) => {
-			if (
-				basepart.Parent &&
-				!basepart.IsDescendantOf(model) &&
-				basepart.Parent.GetAttribute("id") !== undefined &&
-				!results.includes(basepart)
-			) {
-				results.push(basepart);
+		game.GetService("RunService").Heartbeat.Connect(() => {
+			for (const vector of this.points) {
+				Visualize.point(vector, Color3.fromRGB(0, 132, 0));
 			}
 		});
-
-		const returnWorldSpaceMovement = model
-			.GetPivot()
-			.PointToWorldSpace(vector.mul(-1))
-			.sub(model.GetPivot().Position);
-		model.PivotTo(model.GetPivot().add(returnWorldSpaceMovement));
 	}
 
-	static makeJoints(model: Model): void {
-		const part0 = model.PrimaryPart as BasePart;
-		const closestParts = this.getClosestParts(model);
-		for (let i = 0; i < closestParts.size(); i++) {
-			const part1 = closestParts[i];
-			const weld = new Instance("WeldConstraint");
-			weld.Part0 = part0;
-			weld.Part1 = part1;
-			weld.Parent = part0;
-			weld.Name = "AutoWeldConstraint";
+	static getSides(part: BasePart): readonly (readonly [origin: Vector3, normal: Vector3])[] {
+		const get = (sides: readonly Enum.NormalId[]) => {
+			const ret: (readonly [origin: Vector3, normal: Vector3])[] = [];
+			const size = part.Size.mul(0.999);
+
+			for (const side of sides) {
+				const diff = Vector3.FromNormalId(side);
+
+				const push = (x: number, y: number, z: number) => {
+					ret.push([
+						part.CFrame.PointToWorldSpace(new Vector3(x, y, z).sub(part.Size.div(2))),
+						part.CFrame.PointToWorldSpace(diff.mul(0.01)).sub(part.Position),
+					] as const);
+				};
+
+				print(size);
+
+				if (diff.X !== 0) {
+					const my = math.min(size.Y / 2, 1);
+					const mz = math.min(size.Z / 2, 1);
+
+					for (let y = my; y < size.Y; y += 2) {
+						for (let z = mz; z < size.Z; z += 2) {
+							push(diff.X > 0 ? size.X : 0, y, z);
+
+							push(diff.X > 0 ? size.X : 0, y + my, z + mz);
+							push(diff.X > 0 ? size.X : 0, y + my, z - mz);
+							push(diff.X > 0 ? size.X : 0, y - my, z + mz);
+							push(diff.X > 0 ? size.X : 0, y - my, z - mz);
+						}
+					}
+				} else if (diff.Y !== 0) {
+					const mx = math.min(size.X / 2, 1);
+					const mz = math.min(size.Z / 2, 1);
+
+					for (let x = mx; x < size.X; x += 2) {
+						for (let z = mz; z < size.Z; z += 2) {
+							push(x, diff.Y > 0 ? size.Y : 0, z);
+
+							push(x + mx, diff.Y > 0 ? size.Y : 0, z + mz);
+							push(x + mx, diff.Y > 0 ? size.Y : 0, z - mz);
+							push(x - mx, diff.Y > 0 ? size.Y : 0, z + mz);
+							push(x - mx, diff.Y > 0 ? size.Y : 0, z - mz);
+						}
+					}
+				} else if (diff.Z !== 0) {
+					const mx = math.min(size.X / 2, 1);
+					const my = math.min(size.Y / 2, 1);
+
+					for (let x = mx; x < size.X; x += 2) {
+						for (let y = my; y < size.Y; y += 2) {
+							push(x, y, diff.Z > 0 ? size.Z : 0);
+
+							push(x + mx, y + my, diff.Z > 0 ? size.Z : 0);
+							push(x + mx, y - my, diff.Z > 0 ? size.Z : 0);
+							push(x - mx, y + my, diff.Z > 0 ? size.Z : 0);
+							push(x - mx, y - my, diff.Z > 0 ? size.Z : 0);
+						}
+					}
+				}
+			}
+
+			return ret;
+		};
+
+		if (part.IsA("Part")) {
+			if (part.Shape === Enum.PartType.CornerWedge) {
+				return get([Enum.NormalId.Right, Enum.NormalId.Bottom, Enum.NormalId.Front]);
+			} else if (part.Shape === Enum.PartType.Wedge) {
+				return get([Enum.NormalId.Left, Enum.NormalId.Right, Enum.NormalId.Bottom, Enum.NormalId.Back]);
+			}
+		}
+
+		return get([
+			Enum.NormalId.Right,
+			Enum.NormalId.Left,
+
+			Enum.NormalId.Top,
+			Enum.NormalId.Bottom,
+
+			Enum.NormalId.Back,
+			Enum.NormalId.Front,
+		]);
+	}
+
+	static getClosestParts(part: BasePart) {
+		const raycastParams = new RaycastParams();
+		raycastParams.CollisionGroup = "BlockWeld";
+
+		const ret = new Set<BasePart>();
+		for (const [origin, normal] of this.getSides(part)) {
+			this.points.push(origin);
+			const raycastResult = Workspace.Raycast(origin, normal.mul(1), raycastParams);
+			if (!raycastResult) continue;
+
+			ret.add(raycastResult.Instance);
+			//this.points.clear();
+			this.points.push(origin);
+		}
+
+		return ret;
+	}
+
+	static makeJoints(part0: BasePart, part1: BasePart) {
+		const weld = new Instance("WeldConstraint");
+
+		weld.Part0 = part0;
+		weld.Part1 = part1;
+		weld.Name = "AutoWeld";
+
+		weld.Parent = part0;
+	}
+
+	static unweld(model: Model): void {
+		const modelParts = model.GetChildren().filter((value) => value.IsA("BasePart") && value.CanCollide);
+		for (let i = 0; i < modelParts.size(); i++) {
+			const modelPart = modelParts[i] as BasePart;
+			const welds = modelPart.GetJoints();
+			welds.forEach((element) => {
+				element.Destroy();
+			});
 		}
 	}
 
-	static weld(model: Model) {
-		const closestParts = this.getClosestParts(model);
-		closestParts.forEach((element) => {
-			this.makeJoints(element.Parent as Model);
-		});
+	static weld(model: Model): void {
+		const modelParts = model.GetChildren().filter((value) => value.IsA("BasePart") && value.CanCollide);
+		for (let i = 0; i < modelParts.size(); i++) {
+			const modelPart = modelParts[i] as BasePart;
+			const closestParts = this.getClosestParts(modelPart);
+
+			for (const closestPart of closestParts) {
+				if (closestPart.IsDescendantOf(model)) continue;
+
+				this.makeJoints(modelPart, closestPart);
+			}
+		}
 	}
 }
