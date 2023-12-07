11d42a6436c1dc10b6c092b9e6bee1dc21df9087
i3ym <20320769+i3ym@users.noreply.github.com>
2023-12-08 00:30:29 +0700

    Teleport to plot on build mode
    Co-authored-by: Maksim Klimenko <Maks-gaming@users.noreply.github.com>

--- src/client/controller/modes/BuildingMode.ts
+++ src/client/controller/modes/BuildingMode.ts
@@ -1,4 +1,6 @@
+import { Players } from "@rbxts/services";
 import BuildingModeScene, { BuildingModeSceneDefinition } from "client/gui/scenes/BuildingModeScene";
+import SharedPlots from "shared/building/SharedPlots";
 import GuiController from "../GuiController";
 import ToolController from "../ToolController";
 import PlayMode from "./PlayMode";
@@ -22,5 +24,15 @@ export default class BuildingMode extends PlayMode {
 	}
 
 	public onSwitchTo(mode: PlayModes | undefined) {}
-	public onSwitchFrom(prev: PlayModes | undefined) {}
+	public onSwitchFrom(prev: PlayModes | undefined) {
+		if (Players.LocalPlayer.Character) {
+			const plot = SharedPlots.getPlotByOwnerID(Players.LocalPlayer.UserId);
+			const pos = plot.GetPivot().Position.add(new Vector3(plot.GetExtentsSize().X / 2 + 2, 10, 0));
+			const hrp = Players.LocalPlayer.Character.FindFirstChild("HumanoidRootPart") as Part;
+			hrp.CFrame = new CFrame(pos);
+
+			hrp.AssemblyLinearVelocity = Vector3.zero;
+			hrp.AssemblyAngularVelocity = Vector3.zero;
+		}
+	}
 }
