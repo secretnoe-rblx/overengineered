7c46a3fbf3d345db9677a53b6e67c3f9733abec8
i3ym <20320769+i3ym@users.noreply.github.com>
2023-12-19 01:01:11 +0700

    Hide touch control buttons when not in seat

--- src/client/gui/ridemode/RideModeScene.ts
+++ src/client/gui/ridemode/RideModeScene.ts
@@ -208,6 +208,14 @@ export class RideModeControls extends DictionaryControl<RideModeControlsDefiniti
 	start(machine: Machine) {
 		this.clear();
 		machine.destroyed.Connect(() => this.clear());
+		this.event.subscribeObservable(
+			machine.seat.occupiedByLocalPlayer,
+			(occupied) => {
+				if (occupied) this.show();
+				else this.hide();
+			},
+			true,
+		);
 
 		let pos = 0;
 		const map: Record<string, ConfigurableBlockLogic<ConfigValueTypes>[]> = {};
