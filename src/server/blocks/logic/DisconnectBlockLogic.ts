097572eba084bf994a12a642558dd67c21a59a3b
Maks-gaming <77161824+Maks-gaming@users.noreply.github.com>
2024-01-05 14:25:50 +0300

    Rollback old disconnect block logic

--- src/server/blocks/logic/DisconnectBlockLogic.ts
+++ src/server/blocks/logic/DisconnectBlockLogic.ts
@@ -17,7 +17,7 @@ export default class DisconnectBlockLogic {
 				return;
 			}
 
-			(block.FindFirstChild("Ejector") as Part | undefined)?.BreakJoints();
+			(block.FindFirstChild("Ejector") as Part | undefined)?.Destroy();
 		});
 	}
 }
