e1705f7a597da8e45be2c7e85af3dbf1efd241f8
i3ym <20320769+i3ym@users.noreply.github.com>
2024-01-08 03:47:08 +0700

    Hide chat on shiftG

--- src/client/controller/HideInterfaceController.ts
+++ src/client/controller/HideInterfaceController.ts
@@ -1,4 +1,4 @@
-import { StarterGui, UserInputService } from "@rbxts/services";
+import { ReplicatedStorage, StarterGui, UserInputService, Workspace } from "@rbxts/services";
 import GuiController from "./GuiController";
 import PlayModeController from "./PlayModeController";
 
@@ -8,13 +8,32 @@ const initializeHideInterfaceController = (playModeController: PlayModeControlle
 			return;
 		}
 
+		if (UserInputService.GetFocusedTextBox()) {
+			return;
+		}
+
 		const toggle = () => {
 			const gui = GuiController.getGameUI();
 
-			gui.Enabled = !gui.Enabled;
+			// Hide core gui (excluding backpack)
 			StarterGui.SetCoreGuiEnabled(Enum.CoreGuiType.All, gui.Enabled);
 			StarterGui.SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false);
+
+			// Hide screen gui
+			gui.Enabled = !gui.Enabled;
+
+			// Hide mouse cursor
 			UserInputService.MouseIconEnabled = gui.Enabled;
+
+			// Plot owner gui hide
+			Workspace.Plots.GetChildren().forEach((plot) => {
+				const ownerGui = plot.FindFirstChild(ReplicatedStorage.Assets.PlotOwnerGui.Name) as
+					| BillboardGui
+					| undefined;
+				if (ownerGui) {
+					ownerGui.Enabled = gui.Enabled;
+				}
+			});
 		};
 
 		if (input.IsModifierKeyDown("Shift") && UserInputService.IsKeyDown(Enum.KeyCode.G)) {
