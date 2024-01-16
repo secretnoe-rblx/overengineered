daaa4fc36ed75017fc1c87b0dababb0a8e8af87f
Maks-gaming <77161824+Maks-gaming@users.noreply.github.com>
2024-01-16 19:12:53 +0300

    Update DayCycle script

--- src/server/DayCycle.server.ts
+++ src/server/DayCycle.server.ts
@@ -1,29 +1,22 @@
 import { Lighting } from "@rbxts/services";
 
-let minutes = 0;
-let hours = 12;
-let seconds = 0;
+const dayLength = 30;
 
-const secondsPerMinute = 60;
-let curTime = tick();
-let prevTime;
+const cycleTime = dayLength * 60;
+const minutesInADay = 24 * 60;
+
+const startTime = tick() - (Lighting.GetMinutesAfterMidnight() / minutesInADay) * cycleTime;
+let endTime = startTime + cycleTime;
+
+const timeRatio = minutesInADay / cycleTime;
 
 while (true as boolean) {
-	prevTime = curTime;
-	curTime = tick();
-	const dt = curTime - prevTime;
-	seconds = seconds + secondsPerMinute * dt;
-	if (seconds >= 60) {
-		seconds = seconds - 60;
-		minutes = minutes + 1;
-		if (minutes >= 60) {
-			minutes = minutes - 60;
-			hours = hours + 1;
-			if (hours >= 24) {
-				hours = 0;
-			}
-		}
+	const currentTime = tick();
+
+	if (currentTime > endTime) {
+		endTime += cycleTime;
 	}
-	Lighting.TimeOfDay = hours + ":" + math.floor(minutes) + ":" + math.floor(seconds);
-	wait();
+
+	Lighting.SetMinutesAfterMidnight((currentTime - startTime) * timeRatio);
+	wait(1 / 15);
 }
