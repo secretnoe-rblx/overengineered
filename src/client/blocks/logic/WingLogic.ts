66674fb7c498e7df3773a1acceb5cfea15cb1696
Maks-gaming <77161824+Maks-gaming@users.noreply.github.com>
2024-01-19 15:11:54 +0300

    Fixed production aerodynamics (rollback old one)

--- src/client/blocks/logic/WingLogic.ts
+++ src/client/blocks/logic/WingLogic.ts
@@ -1,3 +1,4 @@
+import { RunService } from "@rbxts/services";
 import BlockLogic from "client/base/BlockLogic";
 import { PlacedBlockData } from "shared/building/BlockManager";
 
@@ -18,9 +19,36 @@ export default class WingLogic extends BlockLogic<Wing> {
 		this.wingSurface = this.instance.WingSurface;
 	}
 
+	private findSurface(wingSurface: BasePart): Vector3 {
+		const Z = wingSurface.Size.X * wingSurface.Size.Z;
+		const X = Z * 0.05;
+		return wingSurface.IsA("WedgePart") ? new Vector3(Z, X, X).mul(0.5) : new Vector3(X, Z, X);
+	}
+
 	protected prepare() {
 		super.prepare();
 
-		this.wingSurface.EnableFluidForces = true;
+		// FIXME: Wait for fluid forces to exit Beta
+		if (RunService.IsStudio()) {
+			this.wingSurface.EnableFluidForces = true;
+		} else {
+			const surface = this.findSurface(this.wingSurface);
+			const vectorForce = this.wingSurface.WaitForChild("VectorForce") as VectorForce;
+
+			if (!vectorForce) return;
+
+			this.eventHandler.subscribe(RunService.Heartbeat, () => {
+				const force = surface
+					.mul(
+						this.wingSurface.CFrame.PointToObjectSpace(
+							this.wingSurface.Position.add(this.wingSurface.Velocity),
+						).mul(-1),
+					)
+					.mul(21)
+					.add(vectorForce.Force)
+					.div(2);
+				vectorForce.Force = force;
+			});
+		}
 	}
 }
