1cd365c4d8a9c1894dbd0271fa15cbccc6277c7b
i3ym <20320769+i3ym@users.noreply.github.com>
2024-01-22 21:15:38 +0700

    Fix paint tool physicsx

--- src/shared/building/SharedBuilding.ts
+++ src/shared/building/SharedBuilding.ts
@@ -1,4 +1,5 @@
 import { HttpService } from "@rbxts/services";
+import MaterialPhysicalProperties from "shared/MaterialPhysicalProperties";
 import Serializer from "shared/Serializer";
 import PartUtils from "shared/utils/PartUtils";
 
@@ -20,6 +21,24 @@ export const SharedBuilding = {
 				} else if (!byBuild) {
 					PartUtils.switchDescendantsTransparency(block, 0);
 				}
+
+				// Custom physical properties
+				const customPhysProp =
+					MaterialPhysicalProperties.Properties[data.material.Name] ??
+					MaterialPhysicalProperties.Properties.Default;
+
+				PartUtils.applyToAllDescendantsOfType("BasePart", block, (part) => {
+					if (!part.CustomPhysicalProperties) {
+						const currentPhysProp = part.CurrentPhysicalProperties;
+						part.CustomPhysicalProperties = new PhysicalProperties(
+							customPhysProp.Density ?? currentPhysProp.Density,
+							customPhysProp.Friction ?? currentPhysProp.Friction,
+							customPhysProp.Elasticity ?? currentPhysProp.Elasticity,
+							customPhysProp.FrictionWeight ?? currentPhysProp.FrictionWeight,
+							customPhysProp.ElasticityWeight ?? currentPhysProp.ElasticityWeight,
+						);
+					}
+				});
 			}
 
 			if (data.color) {
