f01d833b4822188913b910fcfe3a616e69d6ecd5
i3ym <20320769+i3ym@users.noreply.github.com>
2024-01-24 00:32:26 +0700

    Optimize distance hide controller

--- src/client/controller/DistanceHideController.client.ts
+++ src/client/controller/DistanceHideController.client.ts
@@ -1,38 +1,40 @@
-import { Players, RunService, Workspace } from "@rbxts/services";
+import { Players, Workspace } from "@rbxts/services";
+import Logger from "shared/Logger";
+import RobloxUnit from "shared/RobloxUnit";
 
-const parts: BasePart[] = [
-	Workspace.FindFirstChild("Baseplate") as BasePart,
-	...Workspace.WaitForChild("Plots")
-		.GetChildren()
-		.map((p) => p.WaitForChild("BuildingArea") as BasePart),
-	...Workspace.WaitForChild("Obstacles")
-		.GetChildren()
-		.filter((p) => p.IsA("BasePart"))
-		.map((p) => p as BasePart),
+// wait for everything to spawn
+task.wait(8);
+
+const parts: Instance[] = [
+	Workspace.WaitForChild("Baseplate"),
+	Workspace.WaitForChild("Plots"),
+	Workspace.WaitForChild("Obstacles"),
 ];
 
-const tr: (BasePart | Decal)[] = [];
-for (const part of parts.map((p) => p.GetDescendants() as (BasePart | Decal)[])) {
+const transparencies = new Map<Instance & { Transparency: number }, number>();
+for (const part of parts.map((p) => [p, ...p.GetDescendants()])) {
 	for (const p of part) {
-		if (p.IsA("BasePart") || p.IsA("Decal")) {
-			tr.push(p);
+		if (p.IsA("BasePart") || p.IsA("Decal") || p.IsA("Texture")) {
+			transparencies.set(p, p.Transparency);
 		}
 	}
 }
 
-const transparencies = new Map([...parts, ...tr].map((p) => [p, p.Transparency]));
+Logger.info(`[DHC] Initializing ${transparencies.size()} instances`);
+const debug = false;
+const maxDistance = RobloxUnit.Meters_To_Studs(1024);
+let visible = true;
 
-RunService.Heartbeat.Connect(() => {
-	const maxDistance = 1024;
+while (true as boolean) {
+	task.wait(1);
 
-	for (const part of parts) {
-		const distance =
-			Players.LocalPlayer.Character?.GetPivot().Position.sub(part.GetPivot().Position).Magnitude ?? 0;
+	const newVisible = (Players.LocalPlayer.Character?.GetPivot().Position.Magnitude ?? 0) < maxDistance;
+	if (visible === newVisible) continue;
 
-		for (const child of [part, ...part.GetDescendants()]) {
-			if (child.IsA("BasePart") || child.IsA("Decal")) {
-				child.Transparency = distance > maxDistance ? 1 : transparencies.get(child) ?? 0;
-			}
-		}
+	visible = newVisible;
+	if (debug) Logger.info(`[DHC] ${visible ? "visible" : "non visible"}`);
+
+	for (const [part, transparency] of transparencies) {
+		part.Transparency = visible ? transparency ?? 0 : 1;
 	}
-});
+}
