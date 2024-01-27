e3abf10a619090f21adb558a7ced9c434cf4e391
Maks-gaming <77161824+Maks-gaming@users.noreply.github.com>
2024-01-27 12:02:41 +0300

    Added player joins logging

--- src/server/AntiRickje.server.ts
+++ src/server/AntiRickje.server.ts
@@ -1,14 +1,18 @@
-import { Players } from "@rbxts/services";
+import { Players, RunService } from "@rbxts/services";
 import DiscordWebhook from "./DiscordWebhook";
 
-const banlist: { [key: number]: string } = {
-	309545016: "apologize and tell me why you did it", // chiseled_cheese
-	19823479: "apologize", // rickje139
-};
+if (!RunService.IsStudio()) {
+	const banlist: { [key: number]: string } = {
+		309545016: "apologize and tell me why you did it", // chiseled_cheese
+		19823479: "apologize", // rickje139
+	};
 
-Players.PlayerAdded.Connect((player) => {
-	if (banlist[player.UserId] !== undefined) {
-		player.Kick(banlist[player.UserId]);
-		DiscordWebhook.log(`${player.Name} tried to join but was kicked by AntiRickje protection`);
-	}
-});
+	Players.PlayerAdded.Connect((player) => {
+		if (banlist[player.UserId] !== undefined) {
+			player.Kick(banlist[player.UserId]);
+			DiscordWebhook.log(`${player.Name} tried to join but was kicked by AntiRickje protection`);
+		} else {
+			DiscordWebhook.log(`${player.Name} joined`);
+		}
+	});
+}
