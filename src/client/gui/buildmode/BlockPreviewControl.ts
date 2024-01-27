c411058ea9b08a350781510f6c5905a6b7fd38fc
i3ym <20320769+i3ym@users.noreply.github.com>
2024-01-28 04:25:30 +0700

    Add gc mode

--- src/client/gui/buildmode/BlockPreviewControl.ts
+++ src/client/gui/buildmode/BlockPreviewControl.ts
@@ -15,10 +15,39 @@ export default class BlockPreviewControl extends Control<ViewportFrame> {
 		const size = this.block.GetExtentsSize();
 		this.block.PivotTo(new CFrame(new Vector3(0, 0.2, -2 - math.max(size.X, size.Y, size.Z))));
 
-		this.event.subscribe(RunService.Heartbeat, () => {
-			this.block.PivotTo(
-				this.block.GetPivot().mul(CFrame.fromEulerAnglesXYZ(math.pi / 180 / 3, math.pi / 180 / 2, 0)),
-			);
-		});
+		const gcmode = false;
+
+		if (!gcmode) {
+			this.event.subscribe(RunService.Heartbeat, (dt) => {
+				this.block.PivotTo(
+					this.block.GetPivot().mul(CFrame.fromEulerAnglesXYZ((math.pi / 3) * dt, (math.pi / 2) * dt, 0)),
+				);
+			});
+		} else {
+			const origin = new CFrame(new Vector3(0, 0.2, -0.5 - math.max(size.X, size.Y, size.Z)));
+			let time = 0;
+
+			this.event.subscribe(RunService.Heartbeat, (dt) => {
+				time += dt;
+
+				if (time < 1) {
+					this.block.PivotTo(
+						origin.add(new Vector3((time - 1) * 4, 0, 0)).mul(CFrame.fromOrientation(0, 60, 0)),
+					);
+				} else if (time < 2) {
+					this.block.PivotTo(
+						origin.add(new Vector3(0, (time - 2) * 4, 0)).mul(CFrame.fromOrientation(0, 0, 60)),
+					);
+				} else if (time < 3) {
+					this.block.PivotTo(
+						origin.add(new Vector3(-(time - 3) * 4, 0, 0)).mul(CFrame.fromOrientation(0, -60, 0)),
+					);
+				} else if (time < 4) {
+					this.block.PivotTo(
+						origin.add(new Vector3(0, -(time - 4) * 4, 0)).mul(CFrame.fromOrientation(0, 0, 0)),
+					);
+				} else time = 0;
+			});
+		}
 	}
 }
