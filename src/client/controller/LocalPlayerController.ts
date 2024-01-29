d565f5149148fea31cb29fd0a60cfefcb9052572
i3ym <20320769+i3ym@users.noreply.github.com>
2024-01-30 04:05:09 +0700

    Integrate sprintcontroller into LocalPlayerController
    Co-authored-by: Maksim Klimenko <Maks-gaming@users.noreply.github.com>

--- src/client/controller/LocalPlayerController.ts
+++ src/client/controller/LocalPlayerController.ts
@@ -1,31 +1,68 @@
-import { Players } from "@rbxts/services";
+import { ContextActionService, Players } from "@rbxts/services";
 import Signals from "client/event/Signals";
 import { PlayerModule } from "client/types/PlayerModule";
+import ObservableValue from "shared/event/ObservableValue";
 import PartUtils from "shared/utils/PartUtils";
+import InputController from "./InputController";
 
 export default class LocalPlayerController {
 	static humanoid: Humanoid | undefined;
 
-	static initialize() {
-		Players.LocalPlayer.CharacterAdded.Connect((_) => {
-			if (!Players.LocalPlayer.HasAppearanceLoaded()) Players.LocalPlayer.CharacterAppearanceLoaded.Wait();
+	// Player settings
+	private static readonly WALKSPEED_DEFAULT = 20;
+	private static readonly WALKSPEED_SPRINT = 60;
 
-			this.onPlayerAdded();
-		});
+	// Properties
+	static readonly isSprinting = new ObservableValue<boolean>(false);
 
-		this.onPlayerAdded();
+	private static playerSpawned() {
+		const character = Players.LocalPlayer.Character!;
+		this.humanoid = character.FindFirstChild("Humanoid") as Humanoid;
+
+		// Death signal event
+		this.humanoid.Died.Once(() => Signals.PLAYER.DIED.Fire());
+
+		// Spawn signal
+		Signals.PLAYER.SPAWN.Fire();
+
+		// Prepare physics
+		this.disableCharacterFluidForces();
 	}
 
-	private static onPlayerAdded() {
-		this.humanoid = Players.LocalPlayer.Character!.WaitForChild("Humanoid") as Humanoid;
+	private static initializeSprintLogic() {
+		// ContextActionService worker
+		const runEvent = (_: string, inputState: Enum.UserInputState, inputObject: InputObject) => {
+			if (inputState !== Enum.UserInputState.Begin) {
+				this.isSprinting.set(false);
+				return Enum.ContextActionResult.Pass;
+			}
+			this.isSprinting.set(true);
+			return Enum.ContextActionResult.Pass;
+		};
 
-		this.humanoid.Died.Once(() => {
-			Signals.PLAYER.DIED.Fire();
+		// Update character walkspeed
+		this.isSprinting.subscribe((value) => {
+			if (!this.humanoid) return;
+
+			this.humanoid.WalkSpeed = value ? this.WALKSPEED_SPRINT : this.WALKSPEED_DEFAULT;
 		});
 
-		this.disableCharacterFluidForces();
+		// Input type handling for displaying sprint button correctly
+		InputController.inputType.subscribe((inputType) => {
+			// Remove old action (if exists)
+			ContextActionService.UnbindAction("Sprint");
 
-		Signals.PLAYER.SPAWN.Fire();
+			// Bind new action
+			ContextActionService.BindAction(
+				"Sprint",
+				runEvent,
+				inputType === "Touch",
+				Enum.KeyCode.LeftShift,
+				Enum.KeyCode.ButtonY,
+			);
+			ContextActionService.SetImage("Sprint", "rbxassetid://9555118706");
+			ContextActionService.SetDescription("Sprint", "Allows you to move more quickly");
+		}, true);
 	}
 
 	/** By default, character has `EnableFluidForces`, but because of the huge `Workspace.AirDensity`, it just flies like a feather */
@@ -35,12 +72,22 @@ export default class LocalPlayerController {
 		});
 	}
 
-	private static getPlayerModuleInstance(): ModuleScript {
-		return Players.LocalPlayer.WaitForChild("PlayerScripts").WaitForChild("PlayerModule") as ModuleScript;
-	}
-
 	/** Native `PlayerModule` library */
 	static getPlayerModule(): PlayerModule {
-		return require(this.getPlayerModuleInstance()) as PlayerModule;
+		const instance = Players.LocalPlayer.WaitForChild("PlayerScripts").WaitForChild("PlayerModule") as ModuleScript;
+		return require(instance) as PlayerModule;
+	}
+
+	static initialize() {
+		this.initializeSprintLogic();
+
+		Players.LocalPlayer.CharacterAdded.Connect((_) => {
+			// Wait for character loading if needed
+			if (!Players.LocalPlayer.HasAppearanceLoaded()) Players.LocalPlayer.CharacterAppearanceLoaded.Wait();
+
+			this.playerSpawned();
+		});
+
+		this.playerSpawned();
 	}
 }
