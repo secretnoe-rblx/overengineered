2e85050dc89d7deec6bdc9e7634d3578b026e679
i3ym <20320769+i3ym@users.noreply.github.com>
2024-02-11 00:35:52 +0700

    Fix player beacon not starting at start

--- src/client/controller/BeaconController.client.ts
+++ src/client/controller/BeaconController.client.ts
@@ -2,11 +2,19 @@ import { Players } from "@rbxts/services";
 import Beacon from "client/gui/Beacon";
 import SharedPlots from "shared/building/SharedPlots";
 
+const createPlayerBeacon = (player: Player) => {
+	while (!player.Character) {
+		task.wait(0.1);
+	}
+
+	new Beacon((player.Character!.WaitForChild("Humanoid") as Humanoid).RootPart!, player.Name, "players").enable();
+};
 Players.PlayerAdded.Connect((player) => {
-	player.CharacterAdded.Connect((char) => {
-		new Beacon((char.WaitForChild("Humanoid") as Humanoid).RootPart!, player.Name, "players").enable();
-	});
+	player.CharacterAdded.Connect(() => createPlayerBeacon(player));
 });
+for (const player of Players.GetPlayers()) {
+	createPlayerBeacon(player);
+}
 
 let plot: PlotModel | undefined;
 while (!plot) {
