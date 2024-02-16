44b7e5366f8d7ff1f1c31476323cadac745229a0
i3ym <20320769+i3ym@users.noreply.github.com>
2024-02-17 03:47:30 +0700

    Destroy suspension beam on clients
    Co-authored-by: Max <Maks-gaming@users.noreply.github.com>

--- src/client/blocks/ServerBlockLogicRegistry.client.ts
+++ src/client/blocks/ServerBlockLogicRegistry.client.ts
@@ -1,10 +1,16 @@
-/*
+import Logger from "shared/Logger";
+import logicRegistry from "shared/block/LogicRegistry";
+import Objects from "shared/fixes/objects";
+import ClientBlockLogic from "./ClientBlockLogic";
+import { SuspensionClientLogic } from "./logic/SuspensionClientLogic";
+
 type ShareableLogic = ExtractMembers<typeof logicRegistry, { readonly clientEvents: Record<string, unknown> }>;
 type ClientBlockLogicRegistry = {
 	readonly [k in keyof ShareableLogic]: new (logic: ShareableLogic[k]) => ClientBlockLogic<ShareableLogic[k]>;
 };
 
 const clientBlockLogicRegistry: ClientBlockLogicRegistry = {
+	suspensionblock: SuspensionClientLogic,
 };
 
 //
@@ -13,4 +19,3 @@ for (const [id, logic] of Objects.pairs(clientBlockLogicRegistry)) {
 	Logger.info(`Initializing client logic for ${id}`);
 	logics.push(new logic(logicRegistry[id] as never));
 }
-*/
--- src/shared/block/logic/SuspensionLogic.ts
+++ src/shared/block/logic/SuspensionLogic.ts
@@ -1,6 +1,7 @@
 import ConfigurableBlockLogic from "shared/block/ConfigurableBlockLogic";
 import blockConfigRegistry from "shared/block/config/BlockConfigRegistry";
 import { PlacedBlockData } from "shared/building/BlockManager";
+import { AutoC2SRemoteEvent } from "shared/event/C2SRemoteEvent";
 
 type Suspension = BlockModel & {
 	readonly SpringSide: BasePart & {
@@ -12,7 +13,13 @@ export default class SuspensionLogic extends ConfigurableBlockLogic<
 	typeof blockConfigRegistry.suspensionblock,
 	Suspension
 > {
-	private springConstraint: SpringConstraint;
+	static readonly clientEvents = {
+		destroy_beam: new AutoC2SRemoteEvent<{
+			readonly block: Suspension;
+		}>("destroy_beam", "UnreliableRemoteEvent"),
+	} as const;
+
+	private readonly springConstraint: SpringConstraint;
 
 	constructor(block: PlacedBlockData) {
 		super(block, blockConfigRegistry.suspensionblock);
@@ -21,5 +28,11 @@ export default class SuspensionLogic extends ConfigurableBlockLogic<
 		this.springConstraint.Damping = this.input.damping.get();
 		this.springConstraint.Stiffness = this.input.stiffness.get();
 		this.event.subscribeObservable(this.input.free_length, (v) => (this.springConstraint.FreeLength = v), true);
+
+		this.onDescendantDestroyed(() => {
+			SuspensionLogic.clientEvents.destroy_beam.send({
+				block: this.instance,
+			});
+		});
 	}
 }
