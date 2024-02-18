5450fa5ffc68e90938742fa15b866a60597291b8
Maks-gaming <77161824+Maks-gaming@users.noreply.github.com>
2024-02-18 18:40:54 +0300

    Fix owner locator local axis

--- src/shared/block/logic/operations/sensors/OwnerLocatorBlockLogic.ts
+++ src/shared/block/logic/operations/sensors/OwnerLocatorBlockLogic.ts
@@ -21,15 +21,14 @@ export default class OwnerLocatorBlockLogic extends ConfigurableBlockLogic<typeo
 		const playerPart = owner.Character?.FindFirstChild("HumanoidRootPart") as Part | undefined;
 		if (!playerPart) return;
 
-		const dir = this.block.instance.GetPivot().PointToObjectSpace(playerPart.Position);
-		this.output.linear.set(dir);
+		const v1 = this.block.instance.GetPivot().PointToObjectSpace(playerPart.Position);
+		const v2 = Vector3.zero;
 
-		const v1 = playerPart.Position;
-		const v2 = this.block.instance.GetPivot().Position;
 		const xAngle = math.atan2(v2.Y - v1.Y, v2.Z - v1.Z);
 		const yAngle = math.atan2(v2.Z - v1.Z, v2.X - v1.X);
 		const zAngle = math.atan2(v2.X - v1.X, v2.Y - v1.Y);
 
+		this.output.linear.set(v1);
 		this.output.angular.set(
 			new Vector3(
 				math.deg(xAngle) > 180 ? math.deg(xAngle) : math.deg(xAngle) - 180,
