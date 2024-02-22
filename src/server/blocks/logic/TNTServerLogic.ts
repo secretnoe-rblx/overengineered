7d67aa86c4663329aa3cf405cb3d3daa446ca3a2
i3ym <20320769+i3ym@users.noreply.github.com>
2024-02-23 03:45:39 +0700

    Improve tnt
    Co-authored-by: Max <Maks-gaming@users.noreply.github.com>

--- src/server/blocks/logic/TNTServerLogic.ts
+++ src/server/blocks/logic/TNTServerLogic.ts
@@ -12,57 +12,59 @@ export default class TNTServerBlockLogic extends ServerBlockLogic<typeof TNTBloc
 
 		logic.events.explode.invoked.Connect((player, { block, isFlammable, pressure, radius }) => {
 			if (!this.isValidBlock(block, player)) return;
+			this.explode(player, block, isFlammable, pressure, radius);
+		});
+	}
 
-			// temporary until block configuration moved to shared
-			radius = math.clamp(radius, 0, 16);
-			pressure = math.clamp(pressure, 0, 2500);
+	explode(player: Player | undefined, block: BlockModel, isFlammable: boolean, pressure: number, radius: number) {
+		// temporary until block configuration moved to shared
+		radius = math.clamp(radius, 0, 16);
+		pressure = math.clamp(pressure, 0, 2500);
 
-			// Explosion
-			const explosion = new Instance("Explosion");
-			explosion.ExplosionType = Enum.ExplosionType.NoCraters;
-			explosion.BlastRadius = radius;
-			explosion.BlastPressure = 0;
-			explosion.DestroyJointRadiusPercent = 0;
-			explosion.Position = block.GetPivot().Position;
+		// Explosion
+		const explosion = new Instance("Explosion");
+		explosion.ExplosionType = Enum.ExplosionType.NoCraters;
+		explosion.BlastRadius = radius;
+		explosion.BlastPressure = 0;
+		explosion.DestroyJointRadiusPercent = 0;
+		explosion.Position = block.GetPivot().Position;
 
-			// Flame explosion
-			if (isFlammable) {
-				const flameExplosion = explosion.Clone();
-				flameExplosion.BlastRadius *= 1.5;
-				flameExplosion.Parent = Workspace;
-				flameExplosion.Hit.Connect((part, distance) => {
-					if (math.random(1, 3) === 1) SpreadingFireController.burn(part);
-				});
+		// Flame explosion
+		if (isFlammable) {
+			const flameExplosion = explosion.Clone();
+			flameExplosion.BlastRadius *= 1.5;
+			flameExplosion.Parent = Workspace;
+			flameExplosion.Hit.Connect((part, distance) => {
+				if (math.random(1, 3) === 1) SpreadingFireController.burn(part);
+			});
+		}
+
+		explosion.Parent = Workspace;
+		explosion.Hit.Connect((part, distance) => {
+			if (!BlockManager.isActiveBlockPart(part)) {
+				return;
 			}
 
-			explosion.Parent = Workspace;
-			explosion.Hit.Connect((part, distance) => {
-				if (!BlockManager.isActiveBlockPart(part)) {
-					return;
-				}
+			if (math.random(1, 2) === 1) {
+				ServerPartUtils.BreakJoints(part);
+			}
 
-				if (math.random(1, 2) === 1) {
-					ServerPartUtils.BreakJoints(part);
-				}
-
-				part.Velocity = new Vector3(
-					math.random(0, pressure / 50),
-					math.random(0, pressure / 50),
-					math.random(0, pressure / 50),
-				);
-			});
-
-			PartUtils.applyToAllDescendantsOfType("Decal", block.PrimaryPart!, (decal) => {
-				decal.Destroy();
-			});
-
-			// Explosion sound
-			const sound = block.PrimaryPart!.FindFirstChild("Sound") as Sound;
-			sound.Play();
-			sound.Ended.Once(() => {
-				sound.Destroy();
-				block.Destroy();
-			});
+			part.Velocity = new Vector3(
+				math.random(0, pressure / 40),
+				math.random(0, pressure / 40),
+				math.random(0, pressure / 40),
+			);
 		});
+
+		block.PrimaryPart!.Transparency = 1;
+		PartUtils.applyToAllDescendantsOfType("Decal", block.PrimaryPart!, (decal) => {
+			decal.Destroy();
+		});
+
+		// Explosion sound
+		const sound = block.PrimaryPart!.FindFirstChild("Sound") as Sound;
+		game.GetService("Debris").AddItem(sound, sound.TimeLength);
+		game.GetService("Debris").AddItem(block, sound.TimeLength);
+		sound.Play();
 	}
 }
