043b61adfd22c70fec57b555e0c1c0c13e5157a5
i3ym <20320769+i3ym@users.noreply.github.com>
2024-02-23 01:35:38 +0700

    Atmosphere ts test
    Co-authored-by: Max <Maks-gaming@users.noreply.github.com>

--- src/shared/types/ReplicatedAssets.d.ts
+++ src/shared/types/ReplicatedAssets.d.ts
@@ -1,5 +1,49 @@
 interface ReplicatedStorage {
 	Assets: {
+		Atmosphere: Folder & {
+			AtmosphericExtinction: Model & {
+				AtmosphericExtinction1: Part & {
+					Attachment1a: Attachment;
+					Attachment2b: Attachment;
+					Attachment3a: Attachment;
+					Attachment4b: Attachment;
+					Attachment5a: Attachment;
+					Attachment6b: Attachment;
+					Attachment7a: Attachment;
+					Attachment8b: Attachment;
+				};
+				AtmosphericExtinction2: Part & {
+					Attachment1b: Attachment;
+					Attachment2a: Attachment;
+					Attachment3b: Attachment;
+					Attachment4a: Attachment;
+					Attachment5b: Attachment;
+					Attachment6a: Attachment;
+					Attachment7b: Attachment;
+					Attachment8a: Attachment;
+
+					Beam1: Beam;
+					Beam2: Beam;
+					Beam3: Beam;
+					Beam4: Beam;
+					Beam5: Beam;
+					Beam6: Beam;
+					Beam7: Beam;
+					Beam8: Beam;
+					Beam9: Beam;
+					Beam10: Beam;
+					Beam11: Beam;
+					Beam12: Beam;
+				};
+			};
+			AtmosphericExtinctionSunset: Model;
+			Sun3D: Part & {
+				Mesh: FileMesh;
+				SunsetLight: BillboardGui & {
+					Light: ImageLabel;
+				};
+			};
+		};
 		Blocks: Folder;
 		CenterOfMass: Model;
 		Axis: Model & {
