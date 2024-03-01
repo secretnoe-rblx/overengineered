d918f87c40cd8fb05b6d06737ce3ac356568bf34
i3ym <20320769+i3ym@users.noreply.github.com>
2024-03-02 03:43:24 +0700

    Optimize magnets
    Co-authored-by: samlovebutter <samlovebutter@users.noreply.github.com>

--- src/shared/block/logic/MagnetBlockLogic.ts
+++ src/shared/block/logic/MagnetBlockLogic.ts
@@ -5,18 +5,40 @@ import { PlacedBlockData } from "shared/building/BlockManager";
 import PartUtils from "shared/utils/PartUtils";
 import VectorUtils from "shared/utils/VectorUtils";
 
+const magicNumber = 60 * 1; // dt
+const magnets: MagnetBlockLogic[] = [];
+const forcesApplied: Map<MagnetBlockLogic, Vector3> = new Map<MagnetBlockLogic, Vector3>();
+
+RunService.PostSimulation.Connect((dt) => {
+	forcesApplied.clear();
+	for (let i = 0; i < magnets.size(); i++) {
+		const strength1 = magnets[i].input.strength.get() * magicNumber * dt;
+		if (strength1 === 0) continue;
+
+		for (let j = i + 1; j < magnets.size(); j++) {
+			const strength2 = magnets[j].input.strength.get() * magicNumber * dt;
+			if (strength2 === 0) continue;
+
+			const calculatedForce: Vector3 = MagnetBlockLogic.calculateForce(magnets[i], magnets[j]);
+			const appliedForce = calculatedForce.mul(strength1).add(calculatedForce.mul(strength2));
+
+			forcesApplied.set(magnets[i], (forcesApplied.get(magnets[i]) ?? Vector3.zero).add(appliedForce));
+			forcesApplied.set(magnets[j], (forcesApplied.get(magnets[j]) ?? Vector3.zero).add(appliedForce.mul(-1)));
+		}
+		magnets[i].part.ApplyImpulseAtPosition(forcesApplied.get(magnets[i])!, magnets[i].part.Position);
+	}
+});
+
 export default class MagnetBlockLogic extends ConfigurableBlockLogic<typeof blockConfigRegistry.magnet> {
-	private static readonly magnets = new Set<MagnetBlockLogic>();
 	private static readonly partColor1 = Color3.fromRGB(128, 128, 240);
 	private static readonly partColor2 = Color3.fromRGB(240, 128, 128);
 
 	private polarity: boolean = false;
-	private readonly part;
+	public readonly part;
 
 	constructor(block: PlacedBlockData) {
 		super(block, blockConfigRegistry.magnet);
 
-		MagnetBlockLogic.magnets.add(this);
 		this.part = this.block.instance.WaitForChild("Part") as BasePart;
 
 		this.event.subscribeObservable(
@@ -32,36 +54,24 @@ export default class MagnetBlockLogic extends ConfigurableBlockLogic<typeof bloc
 			true,
 		);
 
-		//mb do not optimize yet
-		this.event.subscribe(RunService.Heartbeat, () => {
-			const magicNumber = 5;
-			const strength = this.input.strength.get() * magicNumber;
-			if (strength === 0) return;
-			let attractedTo = Vector3.zero;
-			for (const someMagnet of MagnetBlockLogic.magnets) {
-				if (someMagnet === this) continue;
-				const calculatedForce: Vector3 = MagnetBlockLogic.calculateForce(this, someMagnet);
-				attractedTo = attractedTo.add(calculatedForce);
-			}
-
-			const result = attractedTo.mul(strength);
-			this.part.ApplyImpulseAtPosition(result, this.part.Position);
-		});
+		magnets.push(this);
+		forcesApplied.set(this, Vector3.zero);
 	}
 
 	destroy() {
-		MagnetBlockLogic.magnets.delete(this);
+		magnets.remove(magnets.indexOf(this));
+		forcesApplied.delete(this);
 		super.destroy();
 	}
 
-	private static calculateForce(block1: MagnetBlockLogic, block2: MagnetBlockLogic): Vector3 {
+	static calculateForce(block1: MagnetBlockLogic, block2: MagnetBlockLogic): Vector3 {
 		const pos1 = block1.block.instance.GetPivot().Position;
 		const pos2 = block2.block.instance.GetPivot().Position;
 		const difference = pos1.sub(pos2);
 		const distance = difference.Magnitude;
 		if (distance > 10) return Vector3.zero;
 
-		const invSqrt = 1 / (1 + math.pow(distance, 1 / 2));
+		const invSqrt = 1 / (1 + math.sqrt(distance));
 		const isAttracted = block1.polarity === block2.polarity;
 		const result = VectorUtils.normalizeVector3(difference).mul(isAttracted ? invSqrt : -invSqrt);
 
