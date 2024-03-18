7a406ea50fc07455d0e99e8eacf5f91923a7edcb
i3ym <20320769+i3ym@users.noreply.github.com>
2024-03-19 00:31:56 +0700

    Maybe fix slider precision

--- src/shared/event/NumberObservableValue.ts
+++ src/shared/event/NumberObservableValue.ts
@@ -2,9 +2,9 @@ import ObservableValue from "./ObservableValue";
 
 /** ObservableValue that stores a number that can be clamped */
 export default class NumberObservableValue<T extends number | undefined = number> extends ObservableValue<T> {
-	public readonly min;
-	public readonly max;
-	public readonly step;
+	readonly min;
+	readonly max;
+	readonly step;
 
 	constructor(value: T, min: number, max: number, step: number) {
 		super(value);
@@ -14,12 +14,14 @@ export default class NumberObservableValue<T extends number | undefined = number
 		this.step = step;
 	}
 
-	public getRange() {
+	getRange() {
 		return this.max - this.min;
 	}
 
 	protected processValue(value: T) {
 		if (value === undefined) return value;
-		return math.clamp(value - (value % this.step), this.min, this.max) as T;
+
+		const halfstep = this.step / 2;
+		return math.clamp(value - ((value + halfstep) % this.step) + halfstep, this.min, this.max) as T;
 	}
 }
