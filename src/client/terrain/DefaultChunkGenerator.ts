8091accea03c18b455699d43b15dd5c6df13aedb
i3ym <20320769+i3ym@users.noreply.github.com>
2024-04-17 02:49:33 +0700

    Add terrain slope

--- src/client/terrain/DefaultChunkGenerator.ts
+++ src/client/terrain/DefaultChunkGenerator.ts
@@ -1,5 +1,11 @@
+import { Workspace } from "@rbxts/services";
 import { type ChunkGenerator } from "client/terrain/ChunkLoader";
 import { TerrainDataInfo } from "shared/TerrainDataInfo";
+
+const baseplate = Workspace.WaitForChild("Obstacles").WaitForChild("Baseplate") as BasePart;
+const offset = baseplate.Position.div(4);
+const size = baseplate.Size.div(4);
+const slopefunc = (x: number, w: number) => math.max(-math.pow(x / w, -16) + 1, 0);
 
 const terrainData = TerrainDataInfo.data;
 const heightData: Record<number, Record<number, number>> = {};
@@ -15,6 +21,8 @@ export const DefaultChunkGenerator: ChunkGenerator = {
 			const noise = math.noise(x * data[3], data[1], z * data[3]);
 			height += math.clamp(noise, data[4], data[5]) * data[2];
 		}
+
+		height *= math.max(slopefunc(x - offset.X, size.X / 2 - 20), slopefunc(z - offset.Z, size.Z / 2 - 20));
 
 		height += terrainData.shift;
 		height = math.clamp(height, terrainData.minimumHeight, terrainData.maximumHeight);
