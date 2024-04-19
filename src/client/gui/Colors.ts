d906346c9ea9197a18647b771d3bf814601483ea
i3ym <20320769+i3ym@users.noreply.github.com>
2024-04-19 20:58:06 +0700

    Move colors to shared

--- src/client/gui/Colors.ts
+++ src/client/gui/Colors.ts
@@ -1,17 +1,3 @@
-export namespace Colors {
-	export const accentBlack = Color3.fromRGB(12, 12, 20);
-	export const staticBackground = Color3.fromRGB(18, 18, 31);
-	export const accentDark = Color3.fromRGB(58, 58, 100);
-	export const accent = Color3.fromRGB(125, 128, 218); // #7D80DA
-	export const accentLight = Color3.fromRGB(220, 220, 255);
+import { Colors as colors } from "shared/Colors";
 
-	export const white = Color3.fromRGB(255, 255, 255);
-	export const black = Color3.fromRGB(0, 0, 0);
-
-	export const red = Color3.fromRGB(255, 73, 76);
-	export const green = Color3.fromRGB(48, 173, 85);
-	export const blue = Color3.fromRGB(85, 170, 255);
-	export const yellow = Color3.fromRGB(252, 252, 105);
-	export const pink = Color3.fromRGB(244, 142, 255);
-	export const purple = Color3.fromRGB(120, 18, 120);
-}
+export const Colors = colors;
