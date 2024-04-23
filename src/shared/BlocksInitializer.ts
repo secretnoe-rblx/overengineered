9c909e1444433be0b6f177cfd3c55cb7becc0b01
i3ym <20320769+i3ym@users.noreply.github.com>
2024-04-23 18:23:32 +0700

    Add more block checks

--- src/shared/BlocksInitializer.ts
+++ src/shared/BlocksInitializer.ts
@@ -30,6 +30,70 @@ export type BlocksInitializeData = {
 	readonly categories: Writable<Categories>;
 };
 
+namespace Checks {
+	type AssertedModel = Model & { PrimaryPart: BasePart };
+
+	function assertPrimaryPartSet(block: Model): asserts block is AssertedModel {
+		if (!block.PrimaryPart) {
+			throw `PrimaryPart in Block '${block.Name}' is not set!`;
+		}
+	}
+	function assertColboxIsPrimaryPartIfExists(block: AssertedModel) {
+		for (const child of block.GetDescendants()) {
+			if (child.Name.lower() === "colbox") {
+				if (block.PrimaryPart !== child) {
+					throw `Colbox in Block '${block.Name}' is not a primary part!`;
+				}
+			}
+		}
+	}
+	function assertColboxWeldedIfExists(block: AssertedModel) {
+		if (block.PrimaryPart.Name.lower() !== "colbox") return;
+
+		for (const weld of block.GetDescendants()) {
+			if (!weld.IsA("WeldConstraint")) continue;
+
+			if (weld.Part0 === block.PrimaryPart && weld.Part1 && weld.Part1 !== block.PrimaryPart) {
+				return;
+			}
+			if (weld.Part1 === block.PrimaryPart && weld.Part0 && weld.Part0 !== block.PrimaryPart) {
+				return;
+			}
+		}
+
+		throw `Colbox in Block '${block.Name}' is not welded to anything!`;
+	}
+	function assertValidVelds(block: AssertedModel) {
+		for (const weld of block.GetDescendants()) {
+			if (!weld.IsA("WeldConstraint")) continue;
+
+			if (!weld.Part0 || !weld.Part1) {
+				throw `Partial weld found in block ${block.Name} in weld parent ${weld.Parent}`;
+			}
+			if (!weld.Part0.IsDescendantOf(block) || !weld.Part1.IsDescendantOf(block)) {
+				throw `Outer weld reference found in block ${block.Name} in weld parent ${weld.Parent}`;
+			}
+		}
+	}
+	function assertSomethingAnchored(block: AssertedModel) {
+		for (const part of block.GetDescendants()) {
+			if (part.IsA("BasePart") && part.Anchored) {
+				return;
+			}
+		}
+
+		throw `No parts in block '${block.Name}' are anchored!`;
+	}
+
+	export function checkAll(block: Model) {
+		assertPrimaryPartSet(block);
+		assertColboxIsPrimaryPartIfExists(block);
+		assertColboxWeldedIfExists(block);
+		assertValidVelds(block);
+		assertSomethingAnchored(block);
+	}
+}
+
 /** Read blocks and categories from {@link ReplicatedStorage.Assets.Placeable} */
 const readFromAssets = (data: BlocksInitializeData) => {
 	const readCategory = (folder: Folder, prev: Writable<Categories>) => {
@@ -46,23 +110,7 @@ const readFromAssets = (data: BlocksInitializeData) => {
 	};
 
 	const readBlock = (block: Model, categoryName: string) => {
-		if (!block.PrimaryPart) {
-			throw `PrimaryPart in Block '${block.Name}' is not set!`;
-		}
-
-		{
-			let anyAnchored = false;
-			for (const part of block.GetDescendants()) {
-				if (part.IsA("BasePart") && part.Anchored) {
-					anyAnchored = true;
-					break;
-				}
-			}
-
-			if (!anyAnchored) {
-				throw `No parts in block '${block.Name}' are anchored!`;
-			}
-		}
+		Checks.checkAll(block);
 
 		const id = block.Name.lower();
 
