a9c8a48c7cb2cb29d56c65de3142aaff059a7d51
i3ym <20320769+i3ym@users.noreply.github.com>
2024-04-28 00:38:29 +0700

    Wait for game load before starting the tutorial

--- src/client/tutorial/TutorialController.client.ts
+++ src/client/tutorial/TutorialController.client.ts
@@ -1,7 +1,12 @@
+import { Workspace } from "@rbxts/services";
 import { PlayerDataStorage } from "client/PlayerDataStorage";
 import { Tutorial } from "client/tutorial/Tutorial";
 
 while (!PlayerDataStorage.data.get()) {
+	task.wait();
+}
+
+while (!(Workspace.GetAttribute("loaded") as boolean | undefined)) {
 	task.wait();
 }
 
