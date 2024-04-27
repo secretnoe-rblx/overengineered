cc88a30af97dfabf68b7df8f4107dbf4ed2c6095
Maks-gaming <77161824+Maks-gaming@users.noreply.github.com>
2024-04-27 22:39:18 +0300

    version increment

--- src/shared/data/GameDefinitions.ts
+++ src/shared/data/GameDefinitions.ts
@@ -2,7 +2,7 @@ import { RobloxUnit } from "shared/RobloxUnit";
 
 export namespace GameDefinitions {
 	export const APRIL_FOOLS = false;
-	export const VERSION = "BETA v1.0.0";
+	export const VERSION = "BETA v1.0.0a";
 
 	export const GROUP = 1088368;
 	export const RANKS = {
