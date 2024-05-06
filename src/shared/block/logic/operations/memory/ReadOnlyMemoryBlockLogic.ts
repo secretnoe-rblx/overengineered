de85a061d41be9d999e2db52f4c3cd5c207f6b12
i3ym <20320769+i3ym@users.noreply.github.com>
2024-05-07 02:59:45 +0700

    Fix ROM block

--- src/shared/block/logic/operations/memory/ReadOnlyMemoryBlockLogic.ts
+++ src/shared/block/logic/operations/memory/ReadOnlyMemoryBlockLogic.ts
@@ -4,32 +4,28 @@ import { PlacedBlockData } from "shared/building/BlockManager";
 import { RemoteEvents } from "shared/RemoteEvents";
 
 export class ReadOnlyMemoryBlockLogic extends ConfigurableBlockLogic<typeof blockConfigRegistry.readonlymemory> {
-	private readonly size = blockConfigRegistry.readonlymemory.input.menu.lengthLimit;
-	private readonly internalMemory: Parameters<typeof this.output.result.set>[0][] = [];
-
-	private burn() {
-		RemoteEvents.Burn.send([this.instance.PrimaryPart!]);
-	}
+	private readonly LIMIT = blockConfigRegistry.readonlymemory.input.menu.lengthLimit;
 
 	constructor(block: PlacedBlockData) {
 		super(block, blockConfigRegistry.readonlymemory);
-		const mem = this.input.menu.get();
 
-		for (let i = 0; i < this.size; i++) {
-			this.internalMemory[i] = mem[i] ?? 0;
-		}
-
-		const readValue = (address: number) => {
+		const readValue = () => {
 			if (!this.input.read.get()) return;
-			if (address >= this.size || address < 0) {
-				this.burn();
+
+			const address = this.input.address.get();
+			if (address >= this.LIMIT || address < 0) {
+				RemoteEvents.Burn.send([this.instance.PrimaryPart!]);
+				this.disable();
 				return;
 			}
-			const value = this.internalMemory[this.input.address.get()];
+
+			const value = this.input.menu.get()[address] ?? 0;
 			this.output.result.set(value);
-			this.output.size.set(this.internalMemory.size());
 		};
 
 		this.input.address.subscribe(readValue);
+		this.input.read.subscribe(readValue);
+
+		this.event.subscribeObservable(this.input.menu, (menu) => this.output.size.set(menu.size()));
 	}
 }
