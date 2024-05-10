aaad1fb11c5de3e9a7f3a78005f8a0629c38371e
Maks-gaming <77161824+Maks-gaming@users.noreply.github.com>
2024-05-10 16:37:02 +0300

    radio stuff fixes, bytes transferring

--- src/shared/block/logic/RadioRecieverBlockLogic.ts
+++ src/shared/block/logic/RadioRecieverBlockLogic.ts
@@ -4,9 +4,21 @@ import { PlacedBlockData } from "shared/building/BlockManager";
 
 export class RadioRecieverBlockLogic extends ConfigurableBlockLogic<typeof blockConfigRegistry.radioreciever> {
 	static readonly allRecievers = new Map<number, Set<RadioRecieverBlockLogic>>();
+	private readonly colorFade = Color3.fromRGB(0, 0, 0);
+	private readonly originalColor;
+	private readonly led;
+
+	blinkLed() {
+		if (!this.led) return;
+		this.led.Color = this.colorFade;
+		task.delay(0.1, () => (this.led ? (this.led.Color = this.originalColor) : ""));
+	}
 
 	constructor(block: PlacedBlockData) {
 		super(block, blockConfigRegistry.radioreciever);
+
+		this.led = block.instance.WaitForChild("LED") as BasePart;
+		this.originalColor = this.led.Color;
 
 		const changeFrequency = (freq: number, prev: number) => {
 			RadioRecieverBlockLogic.allRecievers.get(prev)?.delete(this);
@@ -15,7 +27,6 @@ export class RadioRecieverBlockLogic extends ConfigurableBlockLogic<typeof block
 			RadioRecieverBlockLogic.allRecievers.get(freq)?.add(this);
 		};
 
-		//no direct reference makes me sad :'(
 		this.event.subscribeObservable(this.input.frequency, changeFrequency);
 	}
 }
--- src/shared/block/logic/RadioTransmitterBlockLogic.ts
+++ src/shared/block/logic/RadioTransmitterBlockLogic.ts
@@ -11,15 +11,33 @@ const sendEvent = new AutoS2CRemoteEvent<{
 		| BlockConfigTypes.Number
 		| BlockConfigTypes.Vec3
 		| BlockConfigTypes.String
+		| BlockConfigTypes.Byte
 	)["config"];
 }>("radio_transmitter_send");
 
 export class RadioTransmitterBlockLogic extends ConfigurableBlockLogic<typeof blockConfigRegistry.radiotransmitter> {
+	private readonly colorFade = Color3.fromRGB(0, 0, 0);
+	private readonly originalColor;
+	private readonly led;
+
+	blinkLed() {
+		if (!this.led) return;
+		this.led.Color = this.colorFade;
+		task.delay(0.1, () => (this.led ? (this.led.Color = this.originalColor) : ""));
+	}
+
 	constructor(block: PlacedBlockData) {
 		super(block, blockConfigRegistry.radiotransmitter);
 
+		this.led = block.instance.WaitForChild("LED") as BasePart;
+		this.originalColor = this.led.Color;
+
 		this.event.subscribe(sendEvent.invoked, ({ frequency, value }) => {
-			RadioRecieverBlockLogic.allRecievers.get(frequency)?.forEach((v) => v.output.value.set(value));
+			this.blinkLed();
+			RadioRecieverBlockLogic.allRecievers.get(frequency)?.forEach((v) => {
+				v.output.value.set(value);
+				v.blinkLed();
+			});
 		});
 
 		this.event.subscribeObservable(this.input.value, (data) => {
