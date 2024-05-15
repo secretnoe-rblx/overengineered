9c767af01e849f3e7c9db0295fa7e436ff7a9682
i3ym <20320769+i3ym@users.noreply.github.com>
2024-05-16 04:29:32 +0700

    Merge lidar and laser
    Co-authored-by: samlovebutter <samlovebutter@users.noreply.github.com>

--- src/shared/block/logic/logic/display/LaserBlockLogic.ts
+++ src/shared/block/logic/logic/display/LaserBlockLogic.ts
@@ -8,6 +8,7 @@ export class LaserBlockLogic extends ConfigurableBlockLogic<typeof blockConfigRe
 	private raycastParams;
 	private ray;
 	private dot;
+	private readonly dotSize = 0.3;
 
 	constructor(block: BlockLogicData<typeof blockConfigRegistry.laser.input>) {
 		super(block, blockConfigRegistry.laser);
@@ -17,6 +18,7 @@ export class LaserBlockLogic extends ConfigurableBlockLogic<typeof blockConfigRe
 		this.dot = this.block.instance.FindFirstChild("Dot") as BasePart;
 		this.ray.Anchored = true;
 		this.dot.Anchored = true;
+		this.dot.Size = new Vector3(this.dotSize, this.dotSize, this.dotSize);
 
 		this.raycastParams = new RaycastParams();
 		this.raycastParams.FilterDescendantsInstances = [this.block.instance];
@@ -24,7 +26,6 @@ export class LaserBlockLogic extends ConfigurableBlockLogic<typeof blockConfigRe
 
 		this.event.subscribe(this.input.rayColor.changed, (color) => (this.ray.Color = color));
 		this.event.subscribe(this.input.dotColor.changed, (color) => (this.dot.Color = color));
-		this.event.subscribe(this.input.dotSize.changed, (size) => (this.dot.Size = new Vector3(size, size, size)));
 	}
 
 	private update() {
@@ -37,19 +38,27 @@ export class LaserBlockLogic extends ConfigurableBlockLogic<typeof blockConfigRe
 		const distance = raycastResult?.Distance ?? this.input.maxDistance.get();
 		const endpos = raycastOrigin.add(this.block.instance.GetPivot().UpVector.mul(distance));
 
-		if (raycastResult || this.input.alwaysEnabled.get()) {
+		this.output.distance.set(
+			raycastResult?.Distance !== undefined ? RobloxUnit.Studs_To_Meters(raycastResult?.Distance) : -1,
+		);
+
+		if (raycastResult?.Distance !== undefined) {
 			this.ray.Transparency = this.input.rayTransparency.get();
 			this.ray.Size = new Vector3(distance, 0.1, 0.1);
 			this.ray.CFrame = new CFrame(raycastOrigin, endpos)
 				.mul(new CFrame(0, 0, -distance / 2))
 				.mul(CFrame.Angles(0, math.rad(90), 0));
 
-			this.dot.Transparency = this.input.dotTransparency.get();
-			this.dot.Size = new Vector3(this.input.dotSize.get(), this.input.dotSize.get(), this.input.dotSize.get());
+			this.dot.Transparency = this.input.rayTransparency.get();
 			this.dot.CFrame = CFrame.lookAlong(endpos, raycastDirection);
 		} else {
 			this.ray.Transparency = 1;
 			this.dot.Transparency = 1;
 		}
+	}
+
+	tick(tick: number): void {
+		this.update();
+		super.tick(tick);
 	}
 }
