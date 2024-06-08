752384ffaf7c53a63aedcb2d56311f2a0aa425ec
i3ym <20320769+i3ym@users.noreply.github.com>
2024-06-09 05:17:50 +0700

    Add animation to dropdown

--- src/client/gui/controls/Dropdown.ts
+++ src/client/gui/controls/Dropdown.ts
@@ -1,22 +1,67 @@
 import { Control } from "client/gui/Control";
 import { ButtonControl } from "client/gui/controls/Button";
+import { TransformService } from "shared/component/TransformService";
+import { ObservableValue } from "shared/event/ObservableValue";
 
 export type DropdownDefinition = GuiObject & {
-	readonly Button: GuiButton;
+	readonly Header: GuiButton & {
+		readonly Arrow?: GuiObject;
+	};
 	readonly Content: GuiObject;
 };
 export class Dropdown<T extends DropdownDefinition = DropdownDefinition> extends Control<T> {
-	private readonly button;
-	private readonly contents;
+	readonly isOpen;
 
-	constructor(gui: T) {
+	constructor(gui: T, defaultVisibility = false) {
 		super(gui);
 
-		this.button = this.add(new ButtonControl(this.gui.Button));
-		this.contents = this.add(new Control(this.gui.Content));
+		this.isOpen = new ObservableValue(defaultVisibility);
 
-		this.event.subscribe(this.button.activated, () => {
-			this.contents.isVisible() ? this.contents.hide() : this.contents.show();
-		});
+		const initVisibilityAnimation = (button: ButtonControl, gui: DropdownDefinition) => {
+			const materialVisibleTransform = TransformService.multi(
+				TransformService.boolStateMachine(
+					gui,
+					TransformService.commonProps.quadOut02,
+					{ Size: gui.Size },
+					{ Size: new UDim2(gui.Size.X, new UDim(0, 40)) },
+				),
+				TransformService.boolStateMachine(
+					gui.Header.FindFirstChild("Arrow") ? gui.Header.Arrow! : new Instance("ImageLabel"),
+					TransformService.commonProps.quadOut02,
+					{ Rotation: 180 },
+					{ Rotation: 0 },
+				),
+			);
+
+			{
+				materialVisibleTransform(false);
+				TransformService.finish(gui);
+
+				if (gui.Header.FindFirstChild("Arrow")) {
+					TransformService.finish(gui.Header.Arrow!);
+				}
+			}
+
+			let wasvisible = defaultVisibility;
+			this.event.subscribe(button.activated, () => {
+				this.isOpen.set(!this.isOpen.get());
+				materialVisibleTransform(this.isOpen.get());
+			});
+
+			this.onDisable(() => {
+				wasvisible = this.isOpen.get();
+				this.isOpen.set(false);
+				materialVisibleTransform(false);
+			});
+			this.onEnable(() => {
+				if (!wasvisible) return;
+
+				this.isOpen.set(true);
+				materialVisibleTransform(true);
+			});
+		};
+
+		const button = this.add(new ButtonControl(this.gui.Header));
+		initVisibilityAnimation(button, gui);
 	}
 }
