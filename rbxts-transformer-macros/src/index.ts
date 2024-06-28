23cd4592af6e5ab8a9c3ffe6828a993a881843db
i3ym <20320769+i3ym@users.noreply.github.com>
2024-06-28 21:06:19 +0700

    Fix compilation on windows

--- rbxts-transformer-macros/src/index.ts
+++ rbxts-transformer-macros/src/index.ts
@@ -200,7 +200,7 @@ const create = (program: ts.Program, context: ts.TransformationContext) => {
 		const identifierByType = (type: ts.Type) => {
 			if (!type?.symbol?.valueDeclaration) return;
 
-			const pth = path.relative("src", type.symbol.valueDeclaration.getSourceFile().fileName)
+			const pth = path.relative("src", type.symbol.valueDeclaration.getSourceFile().fileName).replaceAll("\\", "/");
 			return pth + "$" + type.symbol.name;
 		};
 
