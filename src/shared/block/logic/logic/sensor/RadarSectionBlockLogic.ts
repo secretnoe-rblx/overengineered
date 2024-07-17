bb7d063615095c33c3325ecaffdc132a1bd082ae
Maks-gaming <77161824+Maks-gaming@users.noreply.github.com>
2024-07-18 00:21:25 +0300

    optimized radar section @samlovebutter

--- src/shared/block/logic/logic/sensor/RadarSectionBlockLogic.ts
+++ src/shared/block/logic/logic/sensor/RadarSectionBlockLogic.ts
@@ -1,4 +1,3 @@
-import { Workspace } from "@rbxts/services";
 import { blockConfigRegistry } from "shared/block/config/BlockConfigRegistry";
 import { ConfigurableBlockLogic } from "shared/block/ConfigurableBlockLogic";
 import { RobloxUnit } from "shared/RobloxUnit";
@@ -8,27 +7,20 @@ import type { PlacedBlockData } from "shared/building/BlockManager";
 export class RadarSectionBlockLogic extends ConfigurableBlockLogic<typeof blockConfigRegistry.radarsection> {
 	private closestDetectedPart: BasePart | undefined = undefined;
 	private readonly allTouchedBlocks: Set<BasePart> = new Set<BasePart>();
-	//private justMoved = false;
-	private getDistanceTo(part: BasePart) {
-		if (!part.IsDescendantOf(Workspace)) return;
-		const pivo1 = this.instance.GetPivot();
-		const pivo2 = part.GetPivot();
-		//const distance = .Position.sub(pivo.Position);
-		//const length = distance.Magnitude;
-		//const magic = distance.mul(pivo.XVector);
-		//print(pivo.XVector);
-		const magic = pivo1.ToObjectSpace(pivo2); //ААААААААААААААААА БЛЯЯЯЯЯТЬ
-		//print(magic.Position);
-		//print(vector);
-		return VectorUtils.apply(magic.Position, (v) => RobloxUnit.Studs_To_Meters(v));
-	}
+	private getDistanceTo = (part: BasePart) => {
+		if (this.instance === undefined) return Vector3.zero;
+		if (part === undefined) return Vector3.zero;
+		return VectorUtils.apply(this.instance.GetPivot().ToObjectSpace(part.GetPivot()).Position, (v) =>
+			RobloxUnit.Studs_To_Meters(v),
+		);
+	};
 
 	private findClosestPart() {
 		let smallestDistance: number | undefined;
 		let closestPart: BasePart | undefined;
 		for (const bp of this.allTouchedBlocks) {
-			const d = this.getDistanceTo(bp)?.Magnitude;
-			if (d === undefined) {
+			const d = this.getDistanceTo(bp).Magnitude;
+			if (bp?.Parent === undefined) {
 				this.allTouchedBlocks.delete(bp);
 				continue;
 			}
@@ -39,11 +31,20 @@ export class RadarSectionBlockLogic extends ConfigurableBlockLogic<typeof blockC
 		return closestPart;
 	}
 
+	private isCloser(part: BasePart) {
+		const d1 = this.getDistanceTo(part)?.Magnitude;
+		if (d1 < this.input.minimalDistance.get()) return false;
+		const d2 = this.getDistanceTo(this.closestDetectedPart!)?.Magnitude;
+		return d1 < d2;
+	}
+
 	tick(tick: number): void {
 		super.tick(tick);
 
-		if (this.closestDetectedPart !== undefined)
-			this.output.distance.set(this.getDistanceTo(this.closestDetectedPart) ?? Vector3.zero);
+		if (this.closestDetectedPart === undefined) return;
+		//if (this.allTouchedBlocks.size() === 0) return; //same check?
+		if (this.closestDetectedPart?.Parent === undefined) this.closestDetectedPart = this.findClosestPart();
+		this.output.distance.set(this.getDistanceTo(this.closestDetectedPart!) ?? Vector3.zero);
 	}
 
 	constructor(block: PlacedBlockData) {
@@ -99,26 +100,22 @@ export class RadarSectionBlockLogic extends ConfigurableBlockLogic<typeof blockC
 		*/
 		this.event.subscribe(view.Touched, (part) => {
 			if (part.CollisionGroup !== "Blocks") return;
-			if (part.HasTag("RADARVIEW")) return;
+			//if (part.HasTag("RADARVIEW")) return;
 			if (part.IsDescendantOf(this.instance)) return;
-
-			const d1 = this.getDistanceTo(part)?.Magnitude;
-			if (d1 !== undefined && d1 < this.input.minimalDistance.get()) return;
 			this.allTouchedBlocks.add(part);
 			if (this.closestDetectedPart === undefined) return (this.closestDetectedPart = part);
-			if (this.closestDetectedPart === part) return;
-			const d2 = this.getDistanceTo(this.closestDetectedPart)?.Magnitude;
-			if (d1 === undefined) return;
-			if (d2 === undefined) return (this.closestDetectedPart = part);
-			if (d1 > d2) return;
+			print(this.isCloser(part));
+			if (!this.isCloser(part)) return;
 			this.closestDetectedPart = part;
 		});
 
 		this.event.subscribe(view.TouchEnded, (part) => {
 			this.allTouchedBlocks.delete(part);
 			if (part !== this.closestDetectedPart) return;
-			if ((this.closestDetectedPart = this.findClosestPart()) === undefined)
-				this.output.distance.set(Vector3.zero);
+			this.closestDetectedPart = this.findClosestPart();
+			this.output.distance.set(
+				this.closestDetectedPart ? this.getDistanceTo(this.closestDetectedPart) : Vector3.zero,
+			);
 		});
 
 		this.onDescendantDestroyed(() => {
