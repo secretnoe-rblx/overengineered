902933c31af0d448ccabdc7096905a461d5fbf3d
i3ym <20320769+i3ym@users.noreply.github.com>
2024-07-18 15:19:30 +0700

    Fix dying while building from cache

--- src/client/modes/PlayModeRequest.ts
+++ src/client/modes/PlayModeRequest.ts
@@ -4,10 +4,13 @@ import { Colors } from "client/gui/Colors";
 import { LogControl } from "client/gui/static/LogControl";
 import { CustomRemotes } from "shared/Remotes";
 
+let changing = false;
 export const requestMode = (mode: PlayModes) => {
+	if (changing) return;
 	LoadingController.show(`Changing play mode to ${mode}...`);
 
 	try {
+		changing = true;
 		const response = CustomRemotes.modes.set.send(mode);
 		if (!response.success) {
 			$warn(response.message);
@@ -17,6 +20,7 @@ export const requestMode = (mode: PlayModes) => {
 			return;
 		}
 	} finally {
+		changing = false;
 		LoadingController.hide();
 	}
 };
