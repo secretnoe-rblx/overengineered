4c30de100e527c94aac49c486bac71ecaa2dff6d
Maks-gaming <77161824+Maks-gaming@users.noreply.github.com>
2024-08-15 00:19:05 +0300

    Revert "Maybe radar terrain fix"
    This reverts commit c164a67eeae90573df80e482c38112968435f6f8.

--- src/shared/block/logic/logic/sensor/RadarSectionBlockLogic.ts
+++ src/shared/block/logic/logic/sensor/RadarSectionBlockLogic.ts
@@ -1,12 +1,9 @@
-import { PhysicsService } from "@rbxts/services";
 import { blockConfigRegistry } from "shared/block/config/BlockConfigRegistry";
 import { ConfigurableBlockLogic } from "shared/block/ConfigurableBlockLogic";
 import { RobloxUnit } from "shared/RobloxUnit";
 import { VectorUtils } from "shared/utils/VectorUtils";
 import type { PlacedBlockData } from "shared/building/BlockManager";
 
-PhysicsService.RegisterCollisionGroup("Radar");
-PhysicsService.CollisionGroupSetCollidable("Radar", "Default", false);
 export class RadarSectionBlockLogic extends ConfigurableBlockLogic<typeof blockConfigRegistry.radarsection> {
 	private triggerDistanceListUpdate: boolean = false;
 	private closestDetectedPart: BasePart | undefined = undefined;
@@ -62,8 +59,6 @@ export class RadarSectionBlockLogic extends ConfigurableBlockLogic<typeof blockC
 		if (!view) return;
 		if (!view.IsA("BasePart")) return;
 
-		view.CollisionGroup = "Radar";
-
 		if (!metalPlate) return;
 		if (!metalPlate.IsA("BasePart")) return;
 
@@ -88,6 +83,8 @@ export class RadarSectionBlockLogic extends ConfigurableBlockLogic<typeof blockC
 		});
 
 		this.event.subscribe(view.Touched, (part) => {
+			if (part.IsA("Terrain")) return;
+
 			if (part.CollisionGroup !== "Blocks") return;
 			if (part.HasTag("RADARVIEW")) return;
 			if (this.getDistanceTo(part).Magnitude < this.input.minimalDistance.get()) return;
@@ -97,6 +94,8 @@ export class RadarSectionBlockLogic extends ConfigurableBlockLogic<typeof blockC
 		});
 
 		this.event.subscribe(view.TouchEnded, (part) => {
+			if (part.IsA("Terrain")) return;
+
 			this.allTouchedBlocks.delete(part);
 			this.triggerDistanceListUpdate = part === this.closestDetectedPart;
 		});
