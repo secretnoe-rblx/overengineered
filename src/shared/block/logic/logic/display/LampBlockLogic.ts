9a67172b184440e453233de5983cc56c44e6bc56
samlovebutter <17319180+samlovebutter@users.noreply.github.com>
2024-08-25 23:03:05 +0300

    add configurable point light to lamp

--- src/shared/block/logic/logic/display/LampBlockLogic.ts
+++ src/shared/block/logic/logic/display/LampBlockLogic.ts
@@ -9,18 +9,27 @@ export class LampBlockLogic extends ConfigurableBlockLogic<typeof blockConfigReg
 			readonly block: BlockModel;
 			readonly state: boolean;
 			readonly color: Color3 | undefined;
+			readonly brightness: number;
+			readonly range: number;
 		}>("lamp_update"),
 	} as const;
 
 	constructor(block: PlacedBlockData) {
 		super(block, blockConfigRegistry.lamp);
 
-		this.event.subscribeObservable(this.input.enabled, (enabled) => {
+		const onChange = (isEnableChange: boolean = false) => {
+			if (!isEnableChange && !this.input.enabled.get()) return;
 			LampBlockLogic.events.update.send({
 				block: this.instance,
-				state: enabled === true, // to account the other types
+				state: this.input.enabled.get() === true, // to account the other types
 				color: this.block.color,
+				brightness: this.input.brightness.get() * 0.4, //a.k.a. /100*40
+				range: this.input.lightRrange.get() * 0.6, //a.k.a. /100*60
 			});
-		});
+		};
+
+		this.event.subscribeObservable(this.input.enabled, () => onChange(true));
+		this.event.subscribeObservable(this.input.brightness, () => onChange());
+		this.event.subscribeObservable(this.input.lightRrange, () => onChange());
 	}
 }
