6a9c3a451a49b7f20435c0ddf9fc8b17a641ad5e
Maks-gaming <77161824+Maks-gaming@users.noreply.github.com>
2024-10-02 21:28:42 +0300

    fix for making aero even more realistic lol

--- src/shared/data/GameEnvironment.ts
+++ src/shared/data/GameEnvironment.ts
@@ -3,7 +3,7 @@ import { GameDefinitions } from "shared/data/GameDefinitions";
 
 export namespace GameEnvironment {
 	export const EarthGravity: number = GameDefinitions.APRIL_FOOLS ? 60 : 180;
-	export const EarthAirDensity = 0.05;
+	export const EarthAirDensity = 0.005;
 
 	export const ZeroGravityHeight: number = GameDefinitions.APRIL_FOOLS ? RobloxUnit.Meters_To_Studs(100) : 30000;
 	export const ZeroAirHeight = 20000;
