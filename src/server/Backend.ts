f4e4ef327903d86ea0820babe60335c0675876ae
Maks-gaming <77161824+Maks-gaming@users.noreply.github.com>
2024-10-06 17:08:28 +0300

    Moved stuff to engine & update secrets storing system

--- src/server/Backend.ts
+++ src/server/Backend.ts
@@ -1,4 +1,5 @@
 import { HttpService } from "@rbxts/services";
+import { Secrets } from "engine/server/Secrets";
 
 type Datastore = {
 	name: string;
@@ -17,7 +18,7 @@ type ListEntriesResponse = {
 
 export namespace Backend {
 	const DIST = "https://api.mgcode.ru/roblox";
-	const AccessToken = "d81b0f61-7f62-4016-b5fe-8c5904c9be7d";
+	const AccessToken = Secrets.getSecret("backend_token");
 
 	function getRequest(endpoint: string) {
 		const _data = {
