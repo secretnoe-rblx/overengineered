66d58c48fcced63e65941bd0b1a6ab6d242d806e
i3ym <20320769+i3ym@users.noreply.github.com>
2024-11-07 22:16:42 +0700

    Add set all to scale gui

--- src/client/gui/ScaleEditor.ts
+++ src/client/gui/ScaleEditor.ts
@@ -18,6 +18,10 @@ class NumberControl extends Control<NumberControlDefinition> {
 }
 
 export type ScaleEditorControlDefinition = GuiObject & {
+	readonly ScaleAllControl: GuiObject & {
+		readonly ConfirmButton: GuiButton;
+		readonly ValueTextBox: TextBox;
+	};
 	readonly ScaleXControl: NumberControlDefinition;
 	readonly ScaleYControl: NumberControlDefinition;
 	readonly ScaleZControl: NumberControlDefinition;
@@ -43,5 +47,14 @@ export class ScaleEditorControl extends Control<ScaleEditorControlDefinition> {
 		this.add(new NumberControl(gui.ScaleXControl, createVectorNum("X")));
 		this.add(new NumberControl(gui.ScaleYControl, createVectorNum("Y")));
 		this.add(new NumberControl(gui.ScaleZControl, createVectorNum("Z")));
+
+		const all = this.add(new NumberTextBoxControl(gui.ScaleAllControl.ValueTextBox, 1 / 16, 8));
+		all.value.set(1);
+		this.add(
+			new ButtonControl(gui.ScaleAllControl.ConfirmButton, () => {
+				const val = all.value.get();
+				scale.set(new Vector3(val, val, val));
+			}),
+		);
 	}
 }
