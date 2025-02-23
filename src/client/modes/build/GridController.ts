25df2793920375fe310d76056ac405a00293e35d
i3ym <20320769+i3ym@users.noreply.github.com>
2025-02-24 05:27:01 +0700

    Fix grid controller not showing

--- src/client/modes/build/GridController.ts
+++ src/client/modes/build/GridController.ts
@@ -1,10 +1,8 @@
 import { FloatingWindow } from "client/gui/FloatingWindow";
 import { GridEditorControl } from "client/gui/GridEditor";
-import { Interface } from "client/gui/Interface";
 import { ButtonControl } from "engine/client/gui/Button";
+import { Interface } from "engine/client/gui/Interface";
 import { Component } from "engine/shared/component/Component";
-import { ObjectOverlayStorage } from "engine/shared/component/ObjectOverlayStorage";
-import { Transforms } from "engine/shared/component/Transforms";
 import type { FloatingWindowDefinition } from "client/gui/FloatingWindow";
 import type { GridEditorControlDefinition } from "client/gui/GridEditor";
 import type { MainScreenLayout } from "client/gui/MainScreenLayout";
@@ -21,25 +19,20 @@ export class GridController extends Component {
 	) {
 		super();
 
-		const gridButton = this.parent(mainScreen.registerTopRightButton("Grid"));
+		const gridButton = this.parentGui(mainScreen.registerTopRightButton("Grid"));
 		this.parent(
-			new ButtonControl(
-				gridButton.instance,
-				() => (controlOverlay.get(-1).Visible = !controlOverlay.get(-1).Visible),
+			new ButtonControl(gridButton.instance, () =>
+				floatingScreen.setVisibleAndEnabled(!floatingScreen.isInstanceVisible()),
 			),
 		);
 
-		const floatingGui = Interface.getInterface<{
+		const fg = Interface.getInterface<{
 			Floating: { Grid: FloatingWindowDefinition & { Content: GridEditorControlDefinition } };
 		}>().Floating.Grid;
+		const floatingGui = fg.Clone();
+		floatingGui.Parent = fg.Parent;
 
 		const floatingScreen = this.parent(FloatingWindow.create(floatingGui));
 		floatingScreen.add(new GridEditorControl(floatingGui.Content, moveGrid, rotateGrid, editMode));
-
-		const controlOverlay = ObjectOverlayStorage.transform(
-			floatingGui,
-			{ Visible: false },
-			Transforms.commonProps.quadOut02,
-		);
 	}
 }
