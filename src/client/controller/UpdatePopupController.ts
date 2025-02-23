7b135de800289a4bf44d68d0a40c0f07f4fc217d
i3ym <20320769+i3ym@users.noreply.github.com>
2025-02-24 05:15:29 +0700

    Remove scaling update popup

--- src/client/controller/UpdatePopupController.ts
+++ src/client/controller/UpdatePopupController.ts
@@ -1,4 +1,3 @@
-import { AlertPopup } from "client/gui/popup/AlertPopup";
 import { HostedService } from "engine/shared/di/HostedService";
 import type { PopupController } from "client/gui/PopupController";
 import type { PlayerDataStorage } from "client/PlayerDataStorage";
@@ -14,22 +13,6 @@ export class UpdatePopupController extends HostedService {
 
 			if (lastVersion === 0) {
 				return;
-			}
-
-			if (lastVersion <= 123) {
-				popupController.showPopup(
-					new AlertPopup(
-						`
-Scaling Update 📏
-
-You can now scale blocks! Yes, all of them. At once.
-Up to 8x bigger or 16x smaller.
-To scale blocks use either edit tool (new "scale" option) or build tool (scaling editor in the top right)
-
-You can find more information in our community server.
-					`.trim(),
-					),
-				);
 			}
 		});
 	}
