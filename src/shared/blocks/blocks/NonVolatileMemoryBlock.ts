47354899397d372c189a1bf65ea360940d1e7712
samlovebutter <17319180+samlovebutter@users.noreply.github.com>
2025-03-25 22:54:16 +0300

    ae

---
+++ src/shared/blocks/blocks/NonVolatileMemoryBlock.ts
@@ -0,0 +1,79 @@
+import { Objects } from "engine/shared/fixes/Objects";
+import { BlockLogic } from "shared/blockLogic/BlockLogic";
+import { BlockConfigDefinitions } from "shared/blocks/BlockConfigDefinitions";
+import { BlockCreation } from "shared/blocks/BlockCreation";
+import type { BlockLogicArgs, BlockLogicFullBothDefinitions } from "shared/blockLogic/BlockLogic";
+import type { BlockBuilder } from "shared/blocks/Block";
+
+const definition = {
+	inputOrder: ["set", "value", "defaultValue", "reset"],
+	input: {
+		set: {
+			displayName: "Set",
+			types: {
+				bool: {
+					config: false,
+				},
+			},
+		},
+		value: {
+			displayName: "Value",
+			types: BlockConfigDefinitions.any,
+			group: "1",
+		},
+		defaultValue: {
+			displayName: "Default value",
+			types: BlockConfigDefinitions.any,
+			group: "1",
+			connectorHidden: true,
+		},
+		reset: {
+			displayName: "Reset",
+			tooltip: "Reset the value to the default one",
+			types: BlockConfigDefinitions.bool,
+			configHidden: true,
+		},
+	},
+	output: {
+		result: {
+			displayName: "Result",
+			types: Objects.keys(BlockConfigDefinitions.any),
+			group: "1",
+		},
+	},
+} satisfies BlockLogicFullBothDefinitions;
+
+export type { Logic as LogicMemoryBlockLogic };
+class Logic extends BlockLogic<typeof definition> {
+	constructor(block: BlockLogicArgs) {
+		super(definition, block);
+
+		this.onk(["defaultValue"], ({ defaultValue, defaultValueType, defaultValueChanged }) => {
+			if (!defaultValueChanged) return;
+			this.output.result.set(defaultValueType, defaultValue);
+		});
+		this.onk(["value", "set"], ({ value, valueType, set }) => {
+			if (!set) return;
+			this.output.result.set(valueType, value);
+		});
+
+		this.onk(["reset", "defaultValue"], ({ reset, resetChanged, defaultValue, defaultValueType }) => {
+			if (resetChanged && reset) {
+				this.output.result.set(defaultValueType, defaultValue);
+			}
+		});
+	}
+}
+
+export const LogicMemoryBlock = {
+	...BlockCreation.defaults,
+	id: "logicmemory",
+	displayName: "Memory Cell",
+	description: "Stores the value you gave it",
+
+	logic: { definition, ctor: Logic },
+	modelSource: {
+		model: BlockCreation.Model.fAutoCreated("DoubleGenericLogicBlockPrefab", "MEMORY"),
+		category: () => BlockCreation.Categories.memory,
+	},
+} as const satisfies BlockBuilder;
