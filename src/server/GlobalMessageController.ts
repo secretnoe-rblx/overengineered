78b4b89e61372b8f5e8c0ee2bf8fbb0f40a0562c
i3ym <20320769+i3ym@users.noreply.github.com>
2025-03-28 05:07:23 +0700

    Fix global message double empty

--- src/server/GlobalMessageController.ts
+++ src/server/GlobalMessageController.ts
@@ -1,4 +1,4 @@
-import { MessagingService, RunService } from "@rbxts/services";
+import { MessagingService } from "@rbxts/services";
 import { HostedService } from "engine/shared/di/HostedService";
 import { isNotAdmin_AutoBanned } from "server/BanAdminExploiter";
 import { CustomRemotes } from "shared/Remotes";
@@ -7,37 +7,27 @@ export class GlobalMessageController extends HostedService {
 	constructor() {
 		super();
 
-		this.onEnable(() => {
-			if (!RunService.IsStudio()) {
-				task.spawn(() => {
-					this.event.eventHandler.register(
-						MessagingService.SubscribeAsync("global_message", (message) => {
-							const msg = message as unknown as { text: string; color: Color3; duration: number };
-							CustomRemotes.admin.sendMessage.s2c.send("everyone", {
-								text: msg.text,
-								color: msg.color,
-								duration: msg.duration,
-							});
-						}),
-					);
+		this.event.subscribeRegistration(() =>
+			MessagingService.SubscribeAsync("global_message", ({ Data: message }) => {
+				const msg = message as unknown as { text: string; color: Color3; duration: number };
+				CustomRemotes.admin.sendMessage.s2c.send("everyone", {
+					text: msg.text,
+					color: msg.color,
+					duration: msg.duration,
 				});
-			}
-		});
+			}),
+		);
 
 		CustomRemotes.admin.sendMessage.c2s.invoked.Connect((player, { text, color, duration }) => {
 			if (isNotAdmin_AutoBanned(player, "globalMessage")) {
 				return;
 			}
 
-			task.spawn(() => {
-				MessagingService.PublishAsync("global_message", {
-					text: text,
-					color: color,
-					duration: duration,
-				});
+			MessagingService.PublishAsync("global_message", {
+				text: text,
+				color: color,
+				duration: duration,
 			});
-
-			CustomRemotes.admin.sendMessage.s2c.send("everyone", { text, color, duration });
 		});
 	}
 }
