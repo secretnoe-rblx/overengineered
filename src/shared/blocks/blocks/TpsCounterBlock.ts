326a5a8783f93da85cd6086a844ec80ac68358be
i3ym <20320769+i3ym@users.noreply.github.com>
2025-03-28 15:27:03 +0700

    Update tps counter

--- src/shared/blocks/blocks/TpsCounterBlock.ts
+++ src/shared/blocks/blocks/TpsCounterBlock.ts
@@ -20,20 +20,16 @@ class Logic extends BlockLogic<typeof definition> {
 		super(definition, block);
 
 		let tps = 0;
-		let prevTime = 0;
-		this.onTicc(() => {
-			const time = os.clock();
-			const dt = time - prevTime;
-			prevTime = time;
+		this.onTicc(({ dt }) => {
 			tps = (tps + 1 / dt) / 2;
 
 			// if fps is not a normal number, reset
 			if (tps === math.huge || tps === -math.huge || tps !== tps) {
 				tps = 0;
 			}
-		});
 
-		this.onRecalcInputs(() => this.output.fps.set("number", tps));
+			this.output.fps.set("number", tps);
+		});
 	}
 }
 
