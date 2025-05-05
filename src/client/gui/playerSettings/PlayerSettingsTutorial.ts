aedf38ab34549dac6c948b37251dc972787d1519
i3ym <20320769+i3ym@users.noreply.github.com>
2025-05-05 07:34:18 +0700

    Add tutorials back to settings

--- src/client/gui/playerSettings/PlayerSettingsTutorial.ts
+++ src/client/gui/playerSettings/PlayerSettingsTutorial.ts
@@ -1,24 +1,31 @@
 import { ConfigControlList } from "client/gui/configControls/ConfigControlsList";
+import { BasicCarTutorial } from "client/tutorial/tutorials/BasicCarTutorial";
+import { NewBasicPlaneTutorial } from "client/tutorial/tutorials/NewBasicPlaneTutorial";
 import type {
 	ConfigControlListDefinition,
 	ConfigControlTemplateList,
 } from "client/gui/configControls/ConfigControlsList";
+import type { SettingsPopup } from "client/gui/popup/SettingsPopup";
+import type { TutorialDescriber } from "client/tutorial/TutorialController";
+import type { TutorialsService } from "client/tutorial/TutorialService";
 import type { ObservableValue } from "engine/shared/event/ObservableValue";
 
 export class PlayerSettingsTutorial extends ConfigControlList {
 	constructor(gui: ConfigControlListDefinition & ConfigControlTemplateList, value: ObservableValue<PlayerConfig>) {
 		super(gui);
 
-		this.addCategory("Tutorial; (disabled for the time being)");
-		{
-			// const run = (clazz: ConstructorOf<TutorialDescriber>) => {
-			// 	task.spawn(() => tutorialsService.run(di.resolveForeignClass(clazz)));
-			// 	di.resolve<SettingsPopup>().destroy();
-			// };
-			// this.addButton("Basic plane", () => run(NewBasicPlaneTutorial)) //
-			// 	.setDescription("Basic controls and mechanics");
-			// this.addButton("Basic car", () => run(BasicCarTutorial)) //
-			// 	.setDescription("Basic controls and mechanics");
-		}
+		this.$onInjectAuto((tutorialsService: TutorialsService, settingsPopup: SettingsPopup, di: DIContainer) => {
+			this.addCategory("Tutorial");
+			{
+				const run = (clazz: ConstructorOf<TutorialDescriber>) => {
+					task.spawn(() => tutorialsService.run(di.resolveForeignClass(clazz)));
+					settingsPopup.destroy();
+				};
+				this.addButton("Basic plane", () => run(NewBasicPlaneTutorial)) //
+					.setDescription("Basic controls and mechanics");
+				this.addButton("Basic car", () => run(BasicCarTutorial)) //
+					.setDescription("Basic controls and mechanics");
+			}
+		});
 	}
 }
