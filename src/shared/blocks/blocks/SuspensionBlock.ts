f0bfb5c110f861b986598f34599e5c69fac56e81
i3ym <20320769+i3ym@users.noreply.github.com>
2025-05-23 02:51:49 +0700

    Add limit for suspension block

--- src/shared/blocks/blocks/SuspensionBlock.ts
+++ src/shared/blocks/blocks/SuspensionBlock.ts
@@ -118,6 +118,7 @@ class Logic extends InstanceBlockLogic<typeof definition, SuspensionModel> {
 export const SuspensionBlock = {
 	...BlockCreation.defaults,
 	id: "suspensionblock",
+	limit: 150,
 	displayName: "Suspension",
 	description: "Sus pension spring",
 
