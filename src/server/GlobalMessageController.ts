721a5b97080588e79fa537fece8cbb674ea61a03
samlovebutter <17319180+samlovebutter@users.noreply.github.com>
2025-05-25 16:51:19 +0300

    remove (?) global message controller

--- src/server/GlobalMessageController.ts
+++ src/server/GlobalMessageController.ts
@@ -1,33 +1,34 @@
-import { MessagingService } from "@rbxts/services";
 import { HostedService } from "engine/shared/di/HostedService";
-import { isNotAdmin_AutoBanned } from "server/BanAdminExploiter";
-import { CustomRemotes } from "shared/Remotes";
 
 export class GlobalMessageController extends HostedService {
 	constructor() {
 		super();
 
-		this.event.subscribeRegistration(() =>
-			MessagingService.SubscribeAsync("global_message", ({ Data: message }) => {
-				const msg = message as unknown as { text: string; color: Color3; duration: number };
-				CustomRemotes.admin.sendMessage.s2c.send("everyone", {
-					text: msg.text,
-					color: msg.color,
-					duration: msg.duration,
-				});
-			}),
-		);
+		// task.spawn(() => {
+		// 	this.event.subscribeRegistration(() =>
+		// 		MessagingService.SubscribeAsync("global_message", ({ Data: message }) => {
+		// 			const msg = message as unknown as { text: string; color: Color3; duration: number };
+		// 			CustomRemotes.admin.sendMessage.s2c.send("everyone", {
+		// 				text: msg.text,
+		// 				color: msg.color,
+		// 				duration: msg.duration,
+		// 			});
+		// 		}),
+		// 	);
+		// });
 
-		CustomRemotes.admin.sendMessage.c2s.invoked.Connect((player, { text, color, duration }) => {
-			if (isNotAdmin_AutoBanned(player, "globalMessage")) {
-				return;
-			}
+		// task.spawn(() => {
+		// 	CustomRemotes.admin.sendMessage.c2s.invoked.Connect((player, { text, color, duration }) => {
+		// 		if (isNotAdmin_AutoBanned(player, "globalMessage")) {
+		// 			return;
+		// 		}
 
-			MessagingService.PublishAsync("global_message", {
-				text: text,
-				color: color,
-				duration: duration,
-			});
-		});
+		// 		MessagingService.PublishAsync("global_message", {
+		// 			text: text,
+		// 			color: color,
+		// 			duration: duration,
+		// 		});
+		// 	});
+		// });
 	}
 }
