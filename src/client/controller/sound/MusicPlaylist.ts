42971e3901743d351ac200469a5ad96bf598e98d
samlovebutter <17319180+samlovebutter@users.noreply.github.com>
2025-06-08 21:12:58 +0300

    add volume music setting

--- src/client/controller/sound/MusicPlaylist.ts
+++ src/client/controller/sound/MusicPlaylist.ts
@@ -1,26 +1,32 @@
 import { TweenService } from "@rbxts/services";
 
 export class MusicPlaylist {
-	readonly sounds: Sound[];
+	readonly sounds: { originalVolume: number; sound: Sound }[];
 	private readonly interval: number;
 
 	currentSound: Sound | undefined;
 	private currentSoundEndEvent: RBXScriptConnection | undefined;
 
 	constructor(sounds: Sound[], interval: number) {
-		this.sounds = sounds;
+		this.sounds = sounds.map((v) => ({ originalVolume: v.Volume, sound: v }));
 		this.interval = interval;
 	}
 
 	playSpecificByName(name: string) {
-		this.currentSound = this.sounds.find((v) => v.Name === name);
+		this.currentSound = this.sounds.find((v) => v.sound.Name === name)?.sound;
 		if (!this.currentSound) throw `No music with the name "${name}" was found`;
 		this.currentSound.Play();
 	}
 
+	setVolume(volume: number) {
+		for (const v of this.sounds) {
+			v.sound.Volume = v.originalVolume * volume;
+		}
+	}
+
 	play() {
 		if (this.sounds.isEmpty()) return;
-		this.currentSound = this.sounds[math.random(0, this.sounds.size() - 1)];
+		this.currentSound = this.sounds[math.random(0, this.sounds.size() - 1)]?.sound;
 		this.currentSoundEndEvent = this.currentSound.Ended.Once(() => {
 			wait(this.interval);
 			this.play();
