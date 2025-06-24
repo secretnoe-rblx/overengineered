5d0e47dfe7d47c367d80398be6e514cae24190ca
i3ym <20320769+i3ym@users.noreply.github.com>
2025-06-25 02:05:19 +0700

    Implement image, text blocks preview

--- src/shared/blocks/blocks/gui/GuiImageBlock.ts
+++ src/shared/blocks/blocks/gui/GuiImageBlock.ts
@@ -1,10 +1,18 @@
 import { RunService } from "@rbxts/services";
 import { Colors } from "engine/shared/Colors";
+import { Component } from "engine/shared/component/Component";
 import { Element } from "engine/shared/Element";
 import { BlockLogic } from "shared/blockLogic/BlockLogic";
 import { BlockCreation } from "shared/blocks/BlockCreation";
-import type { RideMode } from "client/modes/ride/RideMode";
-import type { BlockLogicArgs, BlockLogicFullBothDefinitions } from "shared/blockLogic/BlockLogic";
+import { BlockManager } from "shared/building/BlockManager";
+import type { MainScreenLayout } from "client/gui/MainScreenLayout";
+import type { BlockConfigPart } from "shared/blockLogic/BlockConfig";
+import type {
+	BlockLogicArgs,
+	BlockLogicBothDefinitions,
+	BlockLogicFullBothDefinitions,
+} from "shared/blockLogic/BlockLogic";
+import type { BlockLogicTypes } from "shared/blockLogic/BlockLogicTypes";
 import type { BlockBuilder } from "shared/blocks/Block";
 
 const definition = {
@@ -78,14 +86,15 @@ const definition = {
 export type { Logic as SpeakerBlockLogic };
 @injectable
 class Logic extends BlockLogic<typeof definition> {
-	constructor(block: BlockLogicArgs, @inject mode: RideMode) {
+	constructor(block: BlockLogicArgs, @inject screen: MainScreenLayout) {
 		super(definition, block);
 		if (!RunService.IsClient()) return;
 
 		const label = Element.create("ImageLabel", {
 			Name: `BlockImage${block.instance?.Name}`,
 			BackgroundTransparency: 1,
-			Parent: mode.rideModeScene.guiScreen,
+			BorderSizePixel: 0,
+			Parent: screen.fullScreenScaled8,
 		});
 		this.onDisable(() => label.Destroy());
 
@@ -103,13 +112,73 @@ class Logic extends BlockLogic<typeof definition> {
 	}
 }
 
+class PreviewComponent extends Component {
+	constructor(block: BlockModel) {
+		super();
+
+		this.$onInjectAuto((screen: MainScreenLayout) => {
+			type DefinitionToConfig<TDef extends BlockLogicBothDefinitions> = {
+				[k in keyof TDef["input"]]?: BlockConfigPart<
+					(keyof BlockLogicTypes.Primitives & keyof TDef["input"][k]["types"]) | "unset" | "wire"
+				>;
+			};
+
+			const map = <T, V>(value: T, func: (value: T) => V) => func(value);
+
+			const label = Element.create("ImageLabel", {
+				Name: `BlockImagePreview`,
+				BorderSizePixel: 0,
+				Parent: screen.fullScreenScaled8,
+			});
+			this.onDestroy(() => label.Destroy());
+
+			this.event.loop(0.1, () => {
+				const config = (BlockManager.manager.config.get(block) ?? {}) as DefinitionToConfig<typeof definition>;
+
+				label.Image = map(
+					BlockCreation.defaultIfWiredUnset(config.image, definition.input.image.types.string.config),
+					(image) => `rbxassetid://${image}`,
+				);
+				label.Position = map(
+					BlockCreation.defaultIfWiredUnset(config.position, definition.input.position.types.vector3.config),
+					(position) => new UDim2(position.X, 0, position.Y, 0),
+				);
+				label.Size = map(
+					BlockCreation.defaultIfWiredUnset(config.size, definition.input.size.types.vector3.config),
+					(size) => new UDim2(size.X, 0, size.Y, 0),
+				);
+				label.Rotation = BlockCreation.defaultIfWiredUnset(
+					config.rotation,
+					definition.input.rotation.types.number.config,
+				);
+				label.AnchorPoint = map(
+					BlockCreation.defaultIfWiredUnset(config.anchor, definition.input.anchor.types.vector3.config),
+					(anchor) => new Vector2(anchor.X, anchor.Y),
+				);
+				label.BackgroundColor3 = BlockCreation.defaultIfWiredUnset(
+					config.backgroundColor,
+					definition.input.backgroundColor.types.color.config,
+				);
+				label.BackgroundTransparency = BlockCreation.defaultIfWiredUnset(
+					config.backgroundTransparency,
+					definition.input.backgroundTransparency.types.number.config,
+				);
+				label.ZIndex = BlockCreation.defaultIfWiredUnset(
+					config.zIndex,
+					definition.input.zIndex.types.number.config,
+				);
+			});
+		});
+	}
+}
+
 export const GuiImageBlock = {
 	...BlockCreation.defaults,
 	id: "guiimage",
 	displayName: "GUI Image",
 	description: "Draws an image on your screen",
 
-	logic: { definition, ctor: Logic },
+	logic: { definition, ctor: Logic, preview: PreviewComponent },
 	modelSource: {
 		model: BlockCreation.Model.fAutoCreated("x4GenericGuiLogicBlockPrefab", "IMAGE"),
 		category: () => BlockCreation.Categories.gui,
--- src/shared/blocks/blocks/gui/GuiTextBlock.ts
+++ src/shared/blocks/blocks/gui/GuiTextBlock.ts
@@ -1,12 +1,20 @@
 import { RunService } from "@rbxts/services";
 import { Colors } from "engine/shared/Colors";
+import { Component } from "engine/shared/component/Component";
 import { Element } from "engine/shared/Element";
 import { BlockLogic } from "shared/blockLogic/BlockLogic";
 import { BlockConfigDefinitions } from "shared/blocks/BlockConfigDefinitions";
 import { BlockCreation } from "shared/blocks/BlockCreation";
 import { dataToString } from "shared/blocks/blocks/ScreenBlock";
-import type { RideMode } from "client/modes/ride/RideMode";
-import type { BlockLogicArgs, BlockLogicFullBothDefinitions } from "shared/blockLogic/BlockLogic";
+import { BlockManager } from "shared/building/BlockManager";
+import type { MainScreenLayout } from "client/gui/MainScreenLayout";
+import type { BlockConfigPart } from "shared/blockLogic/BlockConfig";
+import type {
+	BlockLogicArgs,
+	BlockLogicBothDefinitions,
+	BlockLogicFullBothDefinitions,
+} from "shared/blockLogic/BlockLogic";
+import type { BlockLogicTypes } from "shared/blockLogic/BlockLogicTypes";
 import type { BlockBuilder } from "shared/blocks/Block";
 
 const definition = {
@@ -96,32 +104,33 @@ const definition = {
 	output: {},
 } satisfies BlockLogicFullBothDefinitions;
 
+const replace = (text: string, str: string, newstr: string) => {
+	const idx = text.find(str)[0];
+	if (idx) {
+		return `${text.sub(1, idx - 1)}${newstr}${text.sub(idx + str.size())}`;
+	}
+
+	return text;
+};
+
 export type { Logic as SpeakerBlockLogic };
 @injectable
 class Logic extends BlockLogic<typeof definition> {
-	constructor(block: BlockLogicArgs, @inject mode: RideMode) {
+	constructor(block: BlockLogicArgs, @inject screen: MainScreenLayout) {
 		super(definition, block);
 		if (!RunService.IsClient()) return;
 
 		const label = Element.create("TextLabel", {
-			Name: `BlockImage${block.instance?.Name}`,
+			Name: `BlockText${block.instance?.Name}`,
 			Font: Enum.Font.Ubuntu,
 			TextColor3: Colors.white,
 			AutomaticSize: Enum.AutomaticSize.XY,
 			BackgroundTransparency: 1,
 			Text: "",
-			Parent: mode.rideModeScene.guiScreen,
+			BorderSizePixel: 0,
+			Parent: screen.fullScreenScaled8,
 		});
 		this.onDisable(() => label.Destroy());
-
-		const replace = (text: string, str: string, newstr: string) => {
-			const idx = text.find(str)[0];
-			if (idx) {
-				return `${text.sub(1, idx - 1)}${newstr}${text.sub(idx + str.size())}`;
-			}
-
-			return text;
-		};
 
 		const format = this.initializeInputCache("format");
 		this.onk(["text"], ({ text }) => (label.Text = replace(format.tryGet() ?? "{}", "{}", dataToString(text))));
@@ -140,13 +149,86 @@ class Logic extends BlockLogic<typeof definition> {
 	}
 }
 
+class PreviewComponent extends Component {
+	constructor(block: BlockModel) {
+		super();
+
+		this.$onInjectAuto((screen: MainScreenLayout) => {
+			type DefinitionToConfig<TDef extends BlockLogicBothDefinitions> = {
+				[k in keyof TDef["input"]]?: BlockConfigPart<
+					(keyof BlockLogicTypes.Primitives & keyof TDef["input"][k]["types"]) | "unset" | "wire"
+				>;
+			};
+
+			const map = <T, V>(value: T, func: (value: T) => V) => func(value);
+
+			const label = Element.create("TextLabel", {
+				Name: `BlockTextPreview`,
+				Font: Enum.Font.Ubuntu,
+				AutomaticSize: Enum.AutomaticSize.XY,
+				BorderSizePixel: 0,
+				Parent: screen.fullScreenScaled8,
+			});
+			this.onDestroy(() => label.Destroy());
+
+			this.event.loop(0.1, () => {
+				const config = (BlockManager.manager.config.get(block) ?? {}) as DefinitionToConfig<typeof definition>;
+
+				label.Text = label.Text = replace(
+					BlockCreation.defaultIfWiredUnset(config.format, definition.input.format.types.string.config),
+					"{}",
+					dataToString(
+						BlockCreation.defaultIfWiredUnset(config.text, definition.input.text.types.string.config),
+					),
+				);
+				label.TextSize = BlockCreation.defaultIfWiredUnset(
+					config.fontSize,
+					definition.input.fontSize.types.number.config,
+				);
+				label.Position = map(
+					BlockCreation.defaultIfWiredUnset(config.position, definition.input.position.types.vector3.config),
+					(position) => new UDim2(position.X, 0, position.Y, 0),
+				);
+				label.Rotation = BlockCreation.defaultIfWiredUnset(
+					config.rotation,
+					definition.input.rotation.types.number.config,
+				);
+				label.AnchorPoint = map(
+					BlockCreation.defaultIfWiredUnset(config.anchor, definition.input.anchor.types.vector3.config),
+					(anchor) => new Vector2(anchor.X, anchor.Y),
+				);
+				label.TextColor3 = BlockCreation.defaultIfWiredUnset(
+					config.textColor,
+					definition.input.textColor.types.color.config,
+				);
+				label.TextTransparency = BlockCreation.defaultIfWiredUnset(
+					config.textTransparency,
+					definition.input.textTransparency.types.number.config,
+				);
+				label.BackgroundColor3 = BlockCreation.defaultIfWiredUnset(
+					config.backgroundColor,
+					definition.input.backgroundColor.types.color.config,
+				);
+				label.BackgroundTransparency = BlockCreation.defaultIfWiredUnset(
+					config.backgroundTransparency,
+					definition.input.backgroundTransparency.types.number.config,
+				);
+				label.ZIndex = BlockCreation.defaultIfWiredUnset(
+					config.zIndex,
+					definition.input.zIndex.types.number.config,
+				);
+			});
+		});
+	}
+}
+
 export const GuiTextBlock = {
 	...BlockCreation.defaults,
 	id: "guitext",
 	displayName: "GUI Text",
 	description: "Writes text on your screen",
 
-	logic: { definition, ctor: Logic },
+	logic: { definition, ctor: Logic, preview: PreviewComponent },
 	modelSource: {
 		model: BlockCreation.Model.fAutoCreated("x4GenericGuiLogicBlockPrefab", "TEXT"),
 		category: () => BlockCreation.Categories.gui,
