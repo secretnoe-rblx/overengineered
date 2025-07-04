998cc2e6aee530839151aa85d14675fe96cf0148
Maks-gaming <77161824+Maks-gaming@users.noreply.github.com>
2025-07-04 19:57:52 +0300

    removed unused & not working service

--- src/server/SandboxGame.ts
+++ src/server/SandboxGame.ts
@@ -10,7 +10,6 @@ import { SlotDatabase } from "server/database/SlotDatabase";
 import { IntegrityCheckerServer } from "server/IntegrityCheckerServer";
 import { PlayModeController as PlayModeController } from "server/modes/PlayModeController";
 import { UnreliableRemoteController } from "server/network/event/UnreliableRemoteHandler";
-import { PlayersCollision } from "server/PlayersCollision";
 import { ServerPlots } from "server/plots/ServerPlots";
 import { RagdollController } from "server/RagdollController";
 import { ServerEffectCreator } from "server/ServerEffectCreator";
@@ -82,7 +81,6 @@ export namespace SandboxGame {
 		builder.services.registerService(ServerBlockLogicController);
 		builder.services.registerService(UnreliableRemoteController);
 		builder.services.registerService(RagdollController);
-		builder.services.registerService(PlayersCollision);
 
 		// BadgeController.initializeIfProd(builder);
 	}
