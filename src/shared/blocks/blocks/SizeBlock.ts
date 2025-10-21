aaf271c07e2a6541ac0fc667da5c2941943081a8
FtRookie <236965164+FtRookie@users.noreply.github.com>
2025-10-21 02:34:04 -0700

    Change description since immediate logic fix

--- src/shared/blocks/blocks/SizeBlock.ts
+++ src/shared/blocks/blocks/SizeBlock.ts
@@ -104,7 +104,7 @@ export const SizeBlock = {
 	...BlockCreation.defaults,
 	id: "sizeblock",
 	displayName: "Size Block",
-	description: "Measure things to your heart's desire, click unit to update",
+	description: "Measure things to your heart's desire, measures end to end.",
 	search: { partialAliases: ["ruler", "length", "width", "height", "measure"] },
 
 	logic: { definition, ctor: Logic, events, immediate },
